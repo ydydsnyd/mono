dedd9229220128fa10ef4ede791dbd88e05b2b75
Aaron Boodman <aaron@aaronboodman.com>
2022-01-11 16:35:58 -1000

    format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,28 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
   },
   rules: {
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/naming-convention': [
-      'error',
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/naming-convention": [
+      "error",
       {
-        selector: 'memberLike',
-        modifiers: ['public'],
-        format: ['camelCase'],
-        leadingUnderscore: 'forbid',
+        selector: "memberLike",
+        modifiers: ["public"],
+        format: ["camelCase"],
+        leadingUnderscore: "forbid",
       },
     ],
-    'eqeqeq': 'error',
-    'no-var': 'error',
-    'object-shorthand': 'error',
-    'prefer-arrow-callback': 'error',
-    'prefer-destructuring': [
-      'error',
+    eqeqeq: "error",
+    "no-var": "error",
+    "object-shorthand": "error",
+    "prefer-arrow-callback": "error",
+    "prefer-destructuring": [
+      "error",
       {
         VariableDeclarator: {
           object: true,
@@ -32,7 +32,7 @@ module.exports = {
         enforceForRenamedProperties: false,
       },
     ],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
 };
