d8a9e848f051bd9b5cc83ecb55b8f0fc608835e7
Greg Baker <greg@roci.dev>
2022-02-17 14:40:00 -0800

    fix: restore export of consoleLogger and type Logger (#23)
    Accidentally deleted by bad merge in 1639e8ba841c208d76b8d243a93b998ae6012bd3

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { Server } from "./server/server.js";
 export { createWorker } from "./server/worker.js";
 export type { Bindings } from "./server/worker.js";
 export type { AuthHandler, UserData } from "./server/auth.js";
+export { consoleLogger, type Logger } from "./util/logger.js";
 export { DatadogLogger } from "./util/datadog-logger.js";
 export { TeeLogger } from "./util/tee-logger.js";
