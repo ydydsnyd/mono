ydydsnyd/mono

commit 5301ccb06e967438f6adb5b198d6a3e905f9d08f

Use MINIFLARE builtin to detct Miniflare

src/server/worker.ts
@@ -79,7 +79,7 @@ async function handleRequest(
   // Send a Sec-WebSocket-Protocol response header with a value
   // matching the Sec-WebSocket-Protocol request header, to indicate
   // support for the protocol, otherwise the client will close the connection.
-  if (env.REQUIRE_SEC_WEBSOCKET_PROTOCOL_RESPONSE_HEADER) {
+  if (typeof MINIFLARE === "undefined") {
     // ...miniflare doesn't like it though. If we set this header under MF,
     // sending the response fails. See:
     // https://github.com/cloudflare/miniflare/issues/179

src/globals.d.ts
@@ -4,6 +4,8 @@ declare global {
   function getMiniflareDurableObjectStorage(
     id: DurableObjectId
   ): Promise<DurableObjectStorage>;
+
+  const MINIFLARE: boolean | undefined;
 }
 
 export {};

commit fdccabfc5b91d3a2eb900efb17f9384f0adb0971

fix: fix worker test (#29)
Broken in 5301ccb06e967438f6adb5b198d6a3e905f9d08f.  We need a way to control MINIFLARE in the tests.  Added some indirection to allow this.

src/server/worker.ts
@@ -5,8 +5,6 @@ declare const MINIFLARE: boolean | undefined;
 
 export interface Bindings {
   server: DurableObjectNamespace;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  REQUIRE_SEC_WEBSOCKET_PROTOCOL_RESPONSE_HEADER?: string;
 }
 
 function createUnauthorizedResponse(message = "Unauthorized"): Response {
@@ -18,7 +16,8 @@ function createUnauthorizedResponse(message = "Unauthorized"): Response {
 async function handleRequest(
   request: Request,
   env: Bindings,
-  authHandler: AuthHandler
+  authHandler: AuthHandler,
+  isMiniflare: boolean
 ): Promise<Response> {
   // Match route against pattern /:name/*action
   const url = new URL(request.url);
@@ -81,7 +80,7 @@ async function handleRequest(
   // Send a Sec-WebSocket-Protocol response header with a value
   // matching the Sec-WebSocket-Protocol request header, to indicate
   // support for the protocol, otherwise the client will close the connection.
-  if (typeof MINIFLARE === "undefined") {
+  if (!isMiniflare) {
     // ...miniflare doesn't like it though. If we set this header under MF,
     // sending the response fails. See:
     // https://github.com/cloudflare/miniflare/issues/179
@@ -100,10 +99,18 @@ async function handleRequest(
 export function createWorker(
   authHandler: AuthHandler
 ): ExportedHandler<Bindings> {
+  return createWorkerInternal(authHandler, typeof MINIFLARE !== "undefined");
+}
+
+// Exported for testing.
+export function createWorkerInternal(
+  authHandler: AuthHandler,
+  isMiniflare: boolean
+): ExportedHandler<Bindings> {
   return {
     fetch: async (request: Request, env: Bindings) => {
       console.debug("handling connection:", request.url);
-      const resp = await handleRequest(request, env, authHandler);
+      const resp = await handleRequest(request, env, authHandler, isMiniflare);
       console.debug(
         `Returning connect response: ${resp.status} ${resp.statusText}`
       );

src/server/worker.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@jest/globals";
 import { encodeHeaderValue } from "../util/headers.js";
 import { Mocket } from "../util/test-utils.js";
 import { USER_DATA_HEADER_NAME } from "./auth";
-import { createWorker } from "./worker";
+import { createWorker, createWorkerInternal } from "./worker";
 
 class TestExecutionContext implements ExecutionContext {
   waitUntil(_promise: Promise<unknown>): void {
@@ -48,8 +48,6 @@ test("worker calls authHandler and sends returned user data in header to DO", as
   const mocket = new Mocket();
 
   const env = {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    REQUIRE_SEC_WEBSOCKET_PROTOCOL_RESPONSE_HEADER: "1",
     server: {
       idFromName: (name: string) => {
         expect(name).toEqual(testRoomID);
@@ -68,30 +66,24 @@ test("worker calls authHandler and sends returned user data in header to DO", as
             expect(request.headers.get("Sec-WebSocket-Protocol")).toEqual(
               encodedTestAuth
             );
-            const headers = new Headers();
-            headers.set("Sec-WebSocket-Protocol", encodedTestAuth);
-            return new Response(null, {
-              status: 101,
-              webSocket: mocket,
-              headers,
-            });
+            return new Response(null, { status: 101, webSocket: mocket });
           },
         };
       },
     },
   };
 
-  const worker = createWorker(async (auth, roomID) => {
+  const workerNotMiniflare = createWorkerInternal(async (auth, roomID) => {
     expect(auth).toEqual(testAuth);
     expect(roomID).toEqual(testRoomID);
     return { userID: auth + ":" + roomID };
-  });
+  }, false);
 
-  if (!worker.fetch) {
+  if (!workerNotMiniflare.fetch) {
     throw new Error("Expect fetch to be defined");
   }
 
-  const response = await worker.fetch(
+  const response = await workerNotMiniflare.fetch(
     testRequest,
     env as unknown as Bindings,
     new TestExecutionContext()
@@ -103,15 +95,21 @@ test("worker calls authHandler and sends returned user data in header to DO", as
     encodedTestAuth
   );
 
-  const env2 = {
-    server: env.server,
-  };
-  const response2 = await worker.fetch(
+  const workerMiniflare = createWorkerInternal(async (auth, roomID) => {
+    expect(auth).toEqual(testAuth);
+    expect(roomID).toEqual(testRoomID);
+    return { userID: auth + ":" + roomID };
+  }, true);
+
+  if (!workerMiniflare.fetch) {
+    throw new Error("Expect fetch to be defined");
+  }
+
+  const response2 = await workerMiniflare.fetch(
     testRequest,
-    env2 as unknown as Bindings,
+    env as unknown as Bindings,
     new TestExecutionContext()
   );
-
   expect(response2.status).toEqual(101);
   expect(response2.webSocket).toBe(mocket);
   expect(response2.headers.get("Sec-WebSocket-Protocol")).toBeNull;