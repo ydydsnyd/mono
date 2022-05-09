db9a5965f5e7e1a75f1323107e8cdcf6b18d8913
Erik Arvidsson <erik.arvidsson@gmail.com>
2022-05-09 17:46:00 +0200

    chore: Fix test runner
    We need to inject process

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,3 +1,4 @@
+// @ts-check
 import {esbuildPlugin} from '@web/dev-server-esbuild';
 import {playwrightLauncher} from '@web/test-runner-playwright';
 
@@ -5,13 +6,19 @@ const chromium = playwrightLauncher({product: 'chromium'});
 const webkit = playwrightLauncher({product: 'webkit'});
 const firefox = playwrightLauncher({product: 'firefox'});
 
-export default {
+/** @type {import('@web/test-runner').TestRunnerConfig} */
+const config = {
   concurrentBrowsers: 3,
   nodeResolve: {
     browser: true,
   },
 
-  plugins: [esbuildPlugin({ts: true, target: 'esnext'})],
+  plugins: [
+    esbuildPlugin({
+      ts: true,
+      target: 'esnext',
+    }),
+  ],
   staticLogging: !!process.env.CI,
   testFramework: {
     config: {
@@ -21,11 +28,16 @@ export default {
       retries: process.env.CI ? 3 : 0, // Firefox is flaky
     },
   },
-  groups: [
-    {
-      name: 'Main',
-      files: ['src/**/*.test.ts'],
-      browsers: [firefox, chromium, webkit],
-    },
-  ],
+  files: ['src/**/*.test.ts'],
+  browsers: [firefox, chromium, webkit],
+  testRunnerHtml: (testFramework) =>
+    `<!doctype html>
+      <html>
+      <body>
+        <script>window.process = { env: { NODE_ENV: "development" } }</script>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
 };
+
+export {config as default};
