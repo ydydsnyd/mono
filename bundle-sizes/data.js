69cb0b586b0779761e57c077e418b181298b84f8
github-action-benchmark <github@users.noreply.github.com>
2022-06-16 01:06:34 +0000

    add Bundle Sizes (customSmallerIsBetter) benchmark result for 89d43f429a4fba16ce63915aea266f605bbaeb47

--- bundle-sizes/data.js
+++ bundle-sizes/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1655324364704,
+  "lastUpdate": 1655341594917,
   "repoUrl": "https://github.com/rocicorp/replicache-internal",
   "entries": {
     "Bundle Sizes": [
@@ -18353,6 +18353,60 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/rocicorp/replicache-internal/commit/74f0db6537efc91d8579e8f81e7a87ec3caabfa6"
         },
         "date": 1655324358474,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Size of replicache.js",
+            "value": 176251,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.js.br (Brotli compressed)",
+            "value": 33047,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.mjs",
+            "value": 175136,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.mjs.br (Brotli compressed)",
+            "value": 32708,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.min.mjs",
+            "value": 79362,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.min.mjs.br (Brotli compressed)",
+            "value": 22998,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aaron@aaronboodman.com",
+            "name": "Aaron Boodman",
+            "username": "aboodman"
+          },
+          "committer": {
+            "email": "aaron@aaronboodman.com",
+            "name": "Aaron Boodman",
+            "username": "aboodman"
+          },
+          "distinct": true,
+          "id": "89d43f429a4fba16ce63915aea266f605bbaeb47",
+          "message": "doc: Add missing id field in get-started doc.\n\nAlso use patch format for some code samples so easier to see what's changing.",
+          "timestamp": "2022-06-15T15:05:04-10:00",
+          "tree_id": "fa6dc48ecab2c9bdd26b82981fa373aa407efe01",
+          "url": "https://github.com/rocicorp/replicache-internal/commit/89d43f429a4fba16ce63915aea266f605bbaeb47"
+        },
+        "date": 1655341591272,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
