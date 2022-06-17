ef4f470d4cf9841ef59ca5ae5cdc4186d9c9ced5
Erik Arvidsson <erik.arvidsson@gmail.com>
2022-06-17 14:46:34 +0200

    chore: Split perf json output (#161)
    This splits the perf json output into two different json files. One for
    p95 and one for the median.

---
+++ tool/split-perf.mjs
@@ -0,0 +1,29 @@
+import {readFile, writeFile} from 'fs/promises';
+import {argv} from 'process';
+
+if (argv.length !== 5) {
+  console.error(
+    'Usage: node split-perf.mjs <input-file> <perf-file> <p95-file>',
+  );
+  process.exit(1);
+}
+
+const inputFile = argv[2];
+const perfFile = argv[3];
+const p95File = argv[4];
+
+const inputJSON = JSON.parse(await readFile(inputFile, 'utf8'));
+
+const perfJSON = [];
+const p95JSON = [];
+
+for (const entry of inputJSON) {
+  if (/p95/.test(entry.unit)) {
+    p95JSON.push(entry);
+  } else {
+    perfJSON.push(entry);
+  }
+}
+
+await writeFile(perfFile, JSON.stringify(perfJSON, null, 2));
+await writeFile(p95File, JSON.stringify(p95JSON, null, 2));
