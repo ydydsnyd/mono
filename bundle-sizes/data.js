272655feff98ff0342b520742c2c39e0113fb858
github-action-benchmark <github@users.noreply.github.com>
2022-12-09 15:33:26 +0000

    add Bundle Sizes (customSmallerIsBetter) benchmark result for 72d9d04d1e9243f257f34a34930fc154f303b11b

--- bundle-sizes/data.js
+++ bundle-sizes/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670598565478,
+  "lastUpdate": 1670600006263,
   "repoUrl": "https://github.com/rocicorp/replicache-internal",
   "entries": {
     "Bundle Sizes": [
@@ -34013,6 +34013,60 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/rocicorp/replicache-internal/commit/39bbab993da340505f3f647372fd93807c70a58f"
         },
         "date": 1670598558705,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Size of replicache.js",
+            "value": 243913,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.js.br (Brotli compressed)",
+            "value": 40437,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.mjs",
+            "value": 242752,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.mjs.br (Brotli compressed)",
+            "value": 40070,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.min.mjs",
+            "value": 98156,
+            "unit": "bytes"
+          },
+          {
+            "name": "Size of replicache.min.mjs.br (Brotli compressed)",
+            "value": 27422,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik.arvidsson@gmail.com",
+            "name": "Erik Arvidsson",
+            "username": "arv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72d9d04d1e9243f257f34a34930fc154f303b11b",
+          "message": "chore: Get rid of more DD31 guards (#478)",
+          "timestamp": "2022-12-09T15:32:29Z",
+          "tree_id": "a84adb2c971188ba1e369486f4144399587aeb02",
+          "url": "https://github.com/rocicorp/replicache-internal/commit/72d9d04d1e9243f257f34a34930fc154f303b11b"
+        },
+        "date": 1670599998856,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
