eec68c6955317ffa1bb36b6cd9893d8008335728
Aaron Boodman <aaron@aaronboodman.com>
2023-02-15 18:53:00 -1000

    Move DatadogClientLogSink into Reflect directly.
    This makes it consistent with the server dd log sink.

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,7 @@
 export {Reflect} from './client/reflect.js';
 export type {ReflectOptions} from './client/options.js';
 export * from 'replicache';
+export {
+  DatadogClientLogSink,
+  type DatadogClientLogSinkOptions,
+} from './client/datadog-client-log-sink.js';
