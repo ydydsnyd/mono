cd6bca485a17e13c1d94c897231d6f69c5557e0f
Jesse Ditson <jesse.ditson@gmail.com>
2023-02-21 18:37:26 -0800

    check in built renderer

---
+++ renderer/pkg/renderer_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function update_cache(a: number, b: number, c: number): void;
+export function draw_buffer_png(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number, r: number, s: number, t: number, u: number, v: number, w: number, x: number, y: number, z: number, a1: number): number;
+export function add_points_to_cache(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number, r: number, s: number, t: number, u: number, v: number, w: number, x: number, y: number, z: number, a1: number, b1: number): void;
+export function draw_buffers(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number, r: number, s: number, t: number, u: number, v: number, w: number, x: number, y: number, z: number, a1: number, b1: number, c1: number, d1: number, e1: number, f1: number): void;
+export function update_state(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number, r: number, s: number, t: number, u: number, v: number, w: number, x: number, y: number, z: number, a1: number, b1: number, c1: number, d1: number, e1: number, f1: number, g1: number, h1: number, i1: number, j1: number, k1: number, l1: number, m1: number, n1: number, o1: number, p1: number, q1: number, r1: number, s1: number): void;
+export function set_physics_state(a: number, b: number, c: number): void;
+export function positions_for_step(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number, o: number, p: number, q: number, r: number, s: number, t: number, u: number, v: number, w: number, x: number, y: number, z: number, a1: number, b1: number, c1: number, d1: number, e1: number, f1: number, g1: number, h1: number, i1: number, j1: number, k1: number, l1: number, m1: number, n1: number, o1: number, p1: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
---
+++ renderer/pkg/snippets/renderer-ba6236b463686f63/src/constants.ts
@@ -0,0 +1,39 @@
+// This is the base UV map size, which is useful for scaling pixel counts.
+export const UVMAP_SIZE = 600;
+
+export const SPLATTER_ANIM_FRAMES = 8;
+
+// This is stored as a flat array so that it can be imported into rust without
+// messing with types or implementing serialization.
+// For each color, there are 6 consecutive bits. The first 3 are the r,g,b of
+// the starting color, and the second 3 are the r,g,b of the ending color.
+export const COLOR_PALATE_RS: number[] = [
+  0.37254902, 0.90980392, 1, 0, 0.77254902, 0.89803922, 0.39215686, 1, 0,
+  0.36862745, 0.85098039, 0.05882353, 0.90588235, 1, 0, 0.83921569, 0.9254902,
+  0, 0.98823529, 0.28627451, 0.67058824, 0.8745098, 0, 0.47843137, 1, 0.6, 0,
+  0.82745098, 0.49803922, 0,
+];
+
+/**
+
+Blue:
+95, 232, 255
+0, 197, 229
+
+Green:
+100, 255, 0
+94, 217, 15
+
+Yellow:
+231, 255, 0
+214, 236, 0
+
+Pink:
+252, 73, 171
+223, 0, 122
+
+Orange:
+255, 153, 0
+211, 127, 0
+
+ */
