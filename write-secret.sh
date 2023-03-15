b21f800e03c77291cef248c95169bd0a91f8abc2
Jesse Ditson <jesse.ditson@gmail.com>
2023-03-15 16:58:11 -0700

    allow empty third arg to write-secret

--- write-secret.sh
+++ write-secret.sh
@@ -2,7 +2,7 @@
 
 [[ -z "$1" ]] && echo "first argument must be a key" && exit 1
 [[ -z "$2" ]] && echo "second argument must be a secret" && exit 1
-[[ "$3" != "staging" ]] && echo "third argument must be empty or 'staging'" && exit 1
+[[ -n "$3" && "$3" != "staging" ]] && echo "third argument must be empty or 'staging'" && exit 1
 
 ENV="rc"
 [[ "$3" == "staging" ]] && ENV="jd"
