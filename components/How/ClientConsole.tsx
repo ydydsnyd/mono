48a9bf962fbef7aa6e9775e7b463d59c372b0eee
Alexander King <alexhking@gmail.com>
2023-03-22 18:07:14 -0400

    Adjusting console styling (#30)

--- components/How/ClientConsole.tsx
+++ components/How/ClientConsole.tsx
@@ -5,6 +5,7 @@ export default class ClientConsole extends React.Component {
     render() {
         return (
             <div className={styles.clientConsole}>
+                <h4 className={styles.panelLabel}>Console</h4>
                 <div className={styles.consoleOutput}>
                     <p className={styles.consoleItem}>
                         Running mutation 1 from client1 on client: 0 → 1
@@ -19,7 +20,6 @@ export default class ClientConsole extends React.Component {
                         Got change of key “foo” on client “c2”: 0 → 1
                     </p>
                 </div>
-                <span className={styles.prompt}>&gt;</span>
             </div>
         );
     }
--- components/How/ServerConsole.tsx
+++ components/How/ServerConsole.tsx
@@ -5,8 +5,11 @@ export default class ServerConsole extends React.Component {
     render() {
         return (
             <div className={styles.serverConsole}>
-                <h4 className={styles.panelLabel}>Server</h4>
+                <h4 className={styles.panelLabel}>Server Console</h4>
                 <div className={styles.consoleOutput}>
+                    <p className={styles.consoleItem}>
+                        Initializing Reflect server
+                    </p>
                     <p className={styles.consoleItem}>
                         Running mutation 1 from client1 on client: 0 → 1
                     </p>
@@ -20,7 +23,6 @@ export default class ServerConsole extends React.Component {
                         Got change of key “foo” on client “c2”: 0 → 1
                     </p>
                 </div>
-                <span className={styles.prompt}>&gt;</span>
             </div>
         );
     }
