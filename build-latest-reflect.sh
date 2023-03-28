91bfaf85e24590b420683e9377f359434f90ce10
Jesse Ditson <jesse.ditson@gmail.com>
2023-03-28 15:16:54 -0700

    use shas for packaged builds

--- build-latest-reflect.sh
+++ build-latest-reflect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VENDOR="$(pwd)/vendor"
-OLD_FILES=$(ls vendor | grep rocicorp-reflect)
+rm ./vendor/rocicorp-reflect-*
 cd ../mono
 [[ $1 == "pull" ]] && git stash -u && git pull && git stash pop
 npm install
@@ -13,21 +13,17 @@ echo "Building and packaging reflect-server"
 npm run build --prefix ./packages/reflect-server -- --debug
 npm pack --workspace ./packages/reflect-server --pack-destination $VENDOR
 REFLECT_SERVER_V=$(npm pkg get version --prefix ./packages/reflect-server | sed 's/"//g')
+BUILT_SHA=$(git rev-parse HEAD)
 cd -
 
 echo "updating packages"
 echo "@rocicorp/reflect $REFLECT_V"
 echo "@rocicorp/reflect-server $REFLECT_SERVER_V"
 
-TIMESTAMP=$(date +%s)
-
-REFLECT_TARBALL_PATH="./vendor/rocicorp-reflect-$REFLECT_V-$TIMESTAMP.tgz"
-REFLECT_SERVER_TARBALL_PATH="./vendor/rocicorp-reflect-server-$REFLECT_SERVER_V-$TIMESTAMP.tgz"
-cp "./vendor/rocicorp-reflect-$REFLECT_V.tgz" $REFLECT_TARBALL_PATH
-cp "./vendor/rocicorp-reflect-server-$REFLECT_SERVER_V.tgz" $REFLECT_SERVER_TARBALL_PATH
-for file in $OLD_FILES; do
-  rm "./vendor/$file"
-done
+REFLECT_TARBALL_PATH="./vendor/rocicorp-reflect-$REFLECT_V-$BUILT_SHA.tgz"
+REFLECT_SERVER_TARBALL_PATH="./vendor/rocicorp-reflect-server-$REFLECT_SERVER_V-$BUILT_SHA.tgz"
+mv "./vendor/rocicorp-reflect-$REFLECT_V.tgz" $REFLECT_TARBALL_PATH
+mv "./vendor/rocicorp-reflect-server-$REFLECT_SERVER_V.tgz" $REFLECT_SERVER_TARBALL_PATH
 
 npm add $REFLECT_TARBALL_PATH
 npm add $REFLECT_SERVER_TARBALL_PATH
