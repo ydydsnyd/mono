e8cd74cea90e8e9415d64e182c2487c11de9cc54
Alexander King <alexhking@gmail.com>
2023-03-29 17:02:08 -0400

    updating favicons for light and dark mode. adding svg with png fallback (#40)

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,12 @@ export default function Home() {
         <meta name="description" content="Reflect" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="favicon.svg" type="image/svg+xml" media="(prefers-color-scheme: light)" />
+        <link rel="icon" href="favicon-dm.svg" type="image/svg+xml" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" media="(prefers-color-scheme: light)" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" media="(prefers-color-scheme: light)" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32-dm.png" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16-dm.png" media="(prefers-color-scheme: dark)" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
