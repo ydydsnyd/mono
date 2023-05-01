d54ac09f0bd6229f26dc186d8f7e8e2d79bd47c9
Aaron Boodman <aaron@aaronboodman.com>
2023-05-01 11:11:58 -1000

    Fix bug in worker host selection on production

--- util/worker-host.ts
+++ util/worker-host.ts
@@ -1,6 +1,6 @@
 export function getWorkerHost() {
   return (
     process.env.NEXT_PUBLIC_WORKER_HOST ||
-    `${location.protocol}//${location.hostname}:8787`.replace(/^http/, 'ws')
-  );
+    `${location.protocol}//${location.hostname}:8787`
+  ).replace(/^http/, 'ws');
 }
