1681a9478d20a32c525ed4d2b45bc88c983eac8d
Alexander King <alexhking@gmail.com>
2023-05-03 17:12:24 -0400

    Adding Google Analytics code (#111)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '@/styles/globals.css';
 import '@/styles/demo.css';
 import type {AppProps} from 'next/app';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  );
 }
