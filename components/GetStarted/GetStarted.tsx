901496e90df3ad189e5a45b5903c54dae652d196
Aaron Boodman <aaron@aaronboodman.com>
2023-05-05 00:29:58 -1000

    more copy

--- components/GetStarted/GetStarted.tsx
+++ components/GetStarted/GetStarted.tsx
@@ -6,20 +6,21 @@ export default function GetStarted() {
   return (
     <div className={styles.getStarted}>
       <p>
-        Reflect is being built by the team behind{' '}
-        <a href="https://replicache.dev/">Replicache</a>, and we&apos;re working
-        toward a public beta later this year.
+        Reflect is being built by <a href="https://rocicorp.dev/">the team</a>{' '}
+        behind <a href="https://replicache.dev/">Replicache</a>, and we&apos;re
+        working toward a public beta later this year.
       </p>
       <p>
-        Want to be first in line? Let us know what you&apos;re building and how you&apos;d use Reflect. We&apos;ll get you access as soon as we can.
+        Want to be first in line? Let us know what you&apos;re building and how
+        you&apos;d use Reflect. We&apos;ll get you access as soon as we can.
       </p>
       <BetaSignup />
       <p className={styles.getStartedNote}>
         You can also contact us by{' '}
         <Link href="mailto:hi@reflect.net">email</Link>, on{' '}
-        <Link href="https://twitter.com/rocicorp">Twitter</Link>, or on <Link href="https://discord.replicache.dev/">Discord</Link>.
+        <Link href="https://twitter.com/rocicorp">Twitter</Link>, or on{' '}
+        <Link href="https://discord.replicache.dev/">Discord</Link>.
       </p>
-      
     </div>
   );
 }
