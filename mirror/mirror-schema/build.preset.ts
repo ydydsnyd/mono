5ab54dbd536e8a66fdda21947620dd63ce9d7d6d
Cesar Alaestante <cesar@roci.dev>
2023-06-07 12:23:56 -0700

    chore: more unbuild mirror schema protocol (#584)

--- mirror/mirror-schema/build.preset.ts
+++ mirror/mirror-schema/build.preset.ts
@@ -3,18 +3,8 @@ import {definePreset} from 'unbuild';
 // @see https://github.com/unjs/unbuild
 export default definePreset({
   clean: true,
-  declaration: true,
-  externals: [
-    'cors',
-    'firebase-functions',
-    'firebase-admin',
-    'body-parser',
-    'busboy',
-    'mirror-protocol',
-    'mirror-schema',
-    'shared',
-  ],
   rollup: {
+    emitCJS: true,
     inlineDependencies: true,
     esbuild: {
       minify: false,
