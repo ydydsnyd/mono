50c3a2ddedc13c18a9fe4d29ef6b213bcba217f7
Erik Arvidsson <erik.arvidsson@gmail.com>
2023-06-15 13:28:50 +0200

    fix: Types for mirror-server (#611)
    Problem
    ---
    
    The problem is that some type definitions for `jest` are implicit which
    requires adding `@types/jest` and some are explicit. But the types in
    the jest libraries are not compatible with the types in the
    `@types/jest` package (when using `exactOptionalPropertyTypes`).
    
    `firestore-jest-mock` is a package that uses implicit dependencies so it
    needs `@types/jest` and causes problems with and without `"types":
    ["jest"]` in `tsconfig.json`.
    
    Solution
    ---
    
    Use a `postinstall` npm script to replace the implicit jest dependencies
    with a explicit one.

---
+++ mirror/mirror-server/fix-firestore-jest-mock.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+// This script fixes the firestore-jest-mock package to use the correct
+// Mock type from jest-mock instead of the one from @types/jest.
+// This is needed because the @types/jest Mock type is not compatible with
+// the jest-mock Mock type.
+
+import {readFileSync, writeFileSync} from 'node:fs';
+import {createRequire} from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+const invalidFiles = [
+  'auth',
+  'fieldValue',
+  'firebase',
+  'firestore',
+  'query',
+  'timestamp',
+  'transaction',
+];
+
+/**
+ * @param {string} basename
+ */
+function fixFile(basename) {
+  const pathName = require.resolve(
+    `firestore-jest-mock/mocks/${basename}.d.ts`,
+  );
+  const content = readFileSync(pathName, 'utf8');
+  const newContent = content.replaceAll(
+    'jest.Mock',
+    `import('jest-mock').Mock`,
+  );
+  if (newContent !== content) {
+    writeFileSync(pathName, newContent);
+  }
+}
+
+for (const basename of invalidFiles) {
+  fixFile(basename);
+}
