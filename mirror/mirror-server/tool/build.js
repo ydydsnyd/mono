5ae96b2abbc575c7833b71ac0ae33a4121e3293f
Erik Arvidsson <erik.arvidsson@gmail.com>
2023-06-21 13:23:10 +0200

    chore(mirror-server): add tool/process-deps.js (#628)
    When deploying to Firebase, Firebase does an `npm install` which tries
    to install our internal packages. That fails because these packages are
    not published to npm. We therefore remove the internal packages from the
    package.json before deploying to firebase.
    
    This is done using predeploy and postdeploy hooks in firebase.json.
    
    We store the list of internal packages in the `bundleDependencies` field
    of package.json. Normally, bundleDependencies is used to specify
    packages that should be bundled into the tarball created by `npm pack`.
    However, we don't use `npm pack` and instead use esbuild to bundle our
    code. We therefore repurpose bundleDependencies to specify packages that
    should be removed from package.json before deploying to Firebase.
    
    Previously we used to not declare these packages as dependencies, but
    that caused problems with turbo repo which failed to build things in the
    right order.

--- mirror/mirror-server/tool/build.js
+++ mirror/mirror-server/tool/build.js
@@ -1,17 +1,24 @@
 import * as esbuild from 'esbuild';
 import packageJSON from '../package.json' assert {type: 'json'};
 
-const {dependencies, devDependencies} = packageJSON;
-const external = Object.keys({
-  ...dependencies,
-  ...devDependencies,
-});
+const {dependencies, devDependencies, bundleDependencies} = packageJSON;
+const external = new Set(
+  Object.keys({
+    ...dependencies,
+    ...devDependencies,
+  }),
+);
+
+// See comment in tool/process-deps.js
+for (const dep of bundleDependencies) {
+  external.delete(dep);
+}
 
 const ctx = await esbuild.context({
   entryPoints: ['src/index.ts'],
   bundle: true,
   outfile: 'out/index.js',
-  external,
+  external: [...external],
   platform: 'node',
   target: 'esnext',
   format: 'esm',
