70cd6412cdfa179dc6dbb2d891ae57ee20fda2db
Erik Arvidsson <arv@roci.dev>
2023-07-19 22:47:07 +0100

    chore: Try using crypto again (#721)
    Seems to work now.

--- mirror/mirror-server/src/test-helpers.ts
+++ mirror/mirror-server/src/test-helpers.ts
@@ -1,4 +1,7 @@
 import {declaredParams} from 'firebase-functions/params';
+import assert from 'node:assert';
+
+assert(process.env.NODE_ENV === 'test', 'Only import this file in tests');
 
 export function mockCloudflareStringParam() {
   for (const p of declaredParams) {
@@ -6,4 +9,15 @@ export function mockCloudflareStringParam() {
       p.value = p.toString = () => 'default-cloudflare-id';
     }
   }
+}
+
+/**
+ * Jest environment does not have crypto defined.
+ */
+export async function installCrypto() {
+  assert(
+    typeof globalThis.crypto === 'undefined',
+    'Only do this if Jest is still broken',
+  );
+  globalThis.crypto = (await import('crypto')).webcrypto as Crypto;
 }
