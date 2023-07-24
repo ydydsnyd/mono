64f0dc729695fa97f1637a39a16891d9d7d6e7a0
d-llama <132324914+d-llama@users.noreply.github.com>
2023-07-24 16:20:43 -0700

    feat!: backfill the version index for FF (#733)

--- packages/reflect-server/src/server/room-schema.ts
+++ packages/reflect-server/src/server/room-schema.ts
@@ -1,9 +1,14 @@
 import type {LogContext} from '@rocicorp/logger';
 import type {DurableStorage} from '../storage/durable-storage.js';
 import {VersionMigrationMap, initStorageSchema} from '../storage/schema.js';
+import {backfillVersionIndex} from '../ff/backfill-version-index.js';
 
 const ROOM_VERSION_MIGRATION_MAP: VersionMigrationMap = {
-  1: {minSafeRollbackVersion: 1}, // The inaugural v1 understands the rollback limit.
+  // The inaugural v1 understands the rollback limit.
+  1: {minSafeRollbackVersion: 1},
+
+  // Initialize / fix version index for Fast(er) Forward.
+  2: backfillVersionIndex,
 };
 
 export async function initRoomSchema(
