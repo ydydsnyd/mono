ba38cfb4d759f11c7023fbf85af7debb59662ca7
d-llama <132324914+d-llama@users.noreply.github.com>
2023-08-24 12:46:49 -0700

    fix(mirror-server): remove unnecessary transaction and obsolete code (#870)

--- mirror/mirror-server/src/cloudflare/get-server-modules.ts
+++ mirror/mirror-server/src/cloudflare/get-server-modules.ts
@@ -1,31 +1,7 @@
 import type {Firestore} from 'firebase-admin/firestore';
-import type {Storage} from 'firebase-admin/storage';
 import {HttpsError} from 'firebase-functions/v2/https';
 import * as schema from 'mirror-schema/src/server.js';
-import assert from 'node:assert';
-import {parseCloudStorageURL} from 'mirror-schema/src/cloud-storage.js';
-import type {CfModule} from './create-script-upload-form.js';
-
-export async function getServerModules(
-  firestore: Firestore,
-  storage: Storage,
-  version: string,
-): Promise<CfModule[]> {
-  const server = await getServerModuleMetadata(firestore, version);
-  const {modules} = server;
-
-  const allModules = await Promise.all(
-    modules.map(async module => {
-      const {name, url, type} = module;
-      const {bucketName, filename} = parseCloudStorageURL(url);
-      const bucket = storage.bucket(bucketName);
-      const content = await bucket.file(filename).download();
-      return {name, content: content[0].toString('utf-8'), type};
-    }),
-  );
-
-  return allModules;
-}
+import {must} from 'shared/src/must.js';
 
 /**
  * Throws an HttpsError if the server module does not exist.
@@ -34,23 +10,13 @@ export async function getServerModuleMetadata(
   firestore: Firestore,
   version: string,
 ): Promise<schema.Server> {
-  const docRef = firestore
+  const doc = await firestore
     .doc(schema.serverPath(version))
-    .withConverter(schema.serverDataConverter);
-
-  const serverModule = await firestore.runTransaction(
-    async txn => {
-      const doc = await txn.get(docRef);
-      const {exists} = doc;
-      if (!exists) {
-        throw new HttpsError('not-found', `Version ${version} does not exist`);
-      }
-
-      return doc.data();
-    },
-    {readOnly: true},
-  );
-  assert(serverModule);
-
-  return serverModule;
+    .withConverter(schema.serverDataConverter)
+    .get();
+  const {exists} = doc;
+  if (!exists) {
+    throw new HttpsError('not-found', `Version ${version} does not exist`);
+  }
+  return must(doc.data());
 }
