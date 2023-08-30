af98ecc02217e0a396c865c2dfcd9fe3a6489c4c
Cesar Alaestante <cesar@roci.dev>
2023-08-30 15:48:25 -0700

    chore: wrangler preview and sandbox (#898)

--- apps/reflect.net/write-secret.sh
+++ apps/reflect.net/write-secret.sh
@@ -2,10 +2,10 @@
 
 [[ -z "$1" ]] && echo "first argument must be a key" && exit 1
 [[ -z "$2" ]] && echo "second argument must be a secret" && exit 1
-[[ -n "$3" && "$3" != "staging" ]] && echo "third argument must be empty or 'staging'" && exit 1
+[[ -n "$3" && "$3" != "sandbox" ]] && echo "third argument must be empty or 'sandbox'" && exit 1
 
 ENV="prod"
-[[ "$3" == "staging" ]] && ENV="staging"
+[[ "$3" == "sandbox" ]] && ENV="sandbox"
 
 F=$(mktemp)
 function cleanup()
