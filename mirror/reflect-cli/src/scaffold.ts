20f1ba26178ec6f41758d9f850012ef846e47aba
d-llama <132324914+d-llama@users.noreply.github.com>
2023-09-04 09:34:12 -0700

    feat(reflect-cli): go back to using the packaged reflect version in "create" (#927)

--- mirror/reflect-cli/src/scaffold.ts
+++ mirror/reflect-cli/src/scaffold.ts
@@ -1,6 +1,9 @@
 import fs, {existsSync} from 'node:fs';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
+import {readFile} from 'node:fs/promises';
+import {pkgUp} from 'pkg-up';
+import {assert, assertObject, assertString} from 'shared/src/asserts.js';
 import {writeTemplatedFilePlaceholders} from './app-config.js';
 
 const templateDir = (templateName: string) =>
@@ -27,9 +30,10 @@ export function copyTemplate(name: string, dest: string) {
   copyDir(sourceDir, dest);
 }
 
-export function scaffold(appName: string, dest: string) {
+export async function scaffold(appName: string, dest: string) {
+  const reflectVersion = await findReflectVersion();
   copyTemplate('create', dest);
-  writeTemplatedFilePlaceholders({appName}, dest, false);
+  writeTemplatedFilePlaceholders({appName, reflectVersion}, dest, false);
 }
 
 function copy(src: string, dest: string) {
@@ -48,4 +52,14 @@ function copyDir(srcDir: string, destDir: string) {
     const destFile = path.resolve(destDir, file);
     copy(srcFile, destFile);
   }
+}
+
+async function findReflectVersion(): Promise<string> {
+  const pkg = await pkgUp({cwd: fileURLToPath(import.meta.url)});
+  assert(pkg);
+  const s = await readFile(pkg, 'utf-8');
+  const v = JSON.parse(s);
+  assertObject(v);
+  assertString(v.version);
+  return v.version;
 }
