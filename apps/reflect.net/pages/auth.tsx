913f0e5cc78a9db5d4d93e473aa5fd6615f196b1
Aaron Boodman <aaron@aaronboodman.com>
2023-09-08 12:18:03 -1000

    feat(mirror): Add some styling to the auth UI.

--- apps/reflect.net/pages/auth.tsx
+++ apps/reflect.net/pages/auth.tsx
@@ -1,6 +1,22 @@
 import {StyledFirebaseAuth} from '@/components/Firebase/StyledFirebaseAuth';
 import {auth, uiConfig} from '@/firebase-config/firebase-auth-ui-config';
+import styles from '@/styles/Auth.module.css';
+import Image from 'next/image';
+import logoAnimated from '@/public/auth/reflect-animated.svg';
 
 export default function Auth() {
-  return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />;
+  return (
+    <div className={styles.authContainer}>
+      <Image
+        src={logoAnimated}
+        loading="lazy"
+        alt="Reflect"
+        className={styles.logoAnimated}
+      />
+      <h3>Sign in to Reflect:</h3>
+      <div className={styles.signinOptions}>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+      </div>
+    </div>
+  );
 }
--- apps/reflect.net/pages/reflect-auth-error.tsx
+++ apps/reflect.net/pages/reflect-auth-error.tsx
@@ -1,3 +1,20 @@
+import styles from '@/styles/Auth.module.css';
+import Image from 'next/image';
+import logoAnimated from '@/public/auth/reflect-animated.svg';
+
 export default function ReflectAuthError() {
-  return <div>Something went wrong with authentication</div>;
+  return (
+    <div className={styles.authContainer}>
+      <Image
+        src={logoAnimated}
+        loading="lazy"
+        alt="Reflect"
+        className={styles.logoAnimated}
+      />
+      <h3>Sign in unsuccessful</h3>
+      <div className={styles.authError}>
+        <span>Something went wrong with authentication</span>
+      </div>
+    </div>
+  );
 }
