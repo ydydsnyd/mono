0252923763299ee264f6156ce11c393db02239b3
Alexander King <alexhking@gmail.com>
2023-10-05 18:16:31 -0700

    First pass at new Get Started section

--- apps/reflect.net/components/GetStarted/GetStarted.tsx
+++ apps/reflect.net/components/GetStarted/GetStarted.tsx
@@ -1,22 +1,37 @@
 import Link from 'next/link';
-import {BetaSignup} from './BetaSignup';
+import Image from 'next/image';
 import styles from './GetStarted.module.css';
+import stopwatchGif from '../../public/img/scaffolding-stopwatch-quickedit.gif'
 
 export function GetStarted() {
   return (
     <div className={styles.getStarted}>
       <p>
-        Reflect is being built by <a href="https://rocicorp.dev/">the team</a>{' '}
-        behind <a href="https://replicache.dev/">Replicache</a>, and we&apos;re
-        working toward a public beta later this year.
+        Reflect is officially in public beta. Give it a try now and build your first multiplayer app in under a minute:
       </p>
+      <div className={styles.imageContainer}>
+        <Link
+          href='https://hello.reflect.net/scaffold'>
+            <Image
+              src={stopwatchGif}
+              alt={`Run Reflect in 41 seconds`}
+              className={styles.innerImage}
+          />
+        </Link>
+        
+      </div>
+      <div className={styles.ctaWrap}>
+        <Link
+          href='https://hello.reflect.net/scaffold'
+          className={styles.buttonPrimary}>
+            Hello, Reflect
+        </Link>
+      </div>
       <p>
-        Want to be first to try it? Let us know what you&apos;re building and
-        how you&apos;d use Reflect. We&apos;ll get you access as soon as we can.
+        Questions?
       </p>
-      <BetaSignup />
       <p className={styles.getStartedNote}>
-        You can also contact us by{' '}
+        You can contact us by{' '}
         <Link href="mailto:hi@reflect.net">email</Link>, on{' '}
         <Link href="https://twitter.com/rocicorp">Twitter</Link>, or on{' '}
         <Link href="https://discord.replicache.dev/">Discord</Link>.
