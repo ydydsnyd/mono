cc25e6f840f6850b9f47c93ada9ff6a0baf7cda4
Alexander King <alexhking@gmail.com>
2023-10-12 15:04:04 -0400

    Prettier fixes

--- apps/reflect.net/components/GetStarted/GetStarted.tsx
+++ apps/reflect.net/components/GetStarted/GetStarted.tsx
@@ -1,30 +1,27 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './GetStarted.module.css';
-import stopwatchGif from '../../public/img/scaffolding-stopwatch-quickedit.gif'
+import stopwatchGif from '../../public/img/scaffolding-stopwatch-quickedit.gif';
 
 export function GetStarted() {
   return (
     <div className={styles.getStarted}>
-      <p>
-        Build your first multiplayer app in under a minute:
-      </p>
+      <p>Build your first multiplayer app in under a minute:</p>
       <div className={styles.imageContainer}>
-        <Link
-          href='https://hello.reflect.net/scaffold'>
-            <Image
-              src={stopwatchGif}
-              alt={`Run Reflect in 41 seconds`}
-              className={styles.innerImage}
+        <Link href="https://hello.reflect.net/scaffold">
+          <Image
+            src={stopwatchGif}
+            alt={`Run Reflect in 41 seconds`}
+            className={styles.innerImage}
           />
         </Link>
-        
       </div>
       <div className={styles.ctaWrap}>
         <Link
-          href='https://hello.reflect.net/scaffold'
-          className={styles.buttonPrimary}>
-            Hello, Reflect
+          href="https://hello.reflect.net/scaffold"
+          className={styles.buttonPrimary}
+        >
+          Hello, Reflect
         </Link>
       </div>
     </div>
