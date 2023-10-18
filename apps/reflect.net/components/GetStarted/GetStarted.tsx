29401e26be138abc9eb669a6d216c7e77f62611f
Alexander King <alexhking@gmail.com>
2023-10-18 10:41:19 -0700

    Adjusting get started section styling (#1117)
    * Adjusting get started section styling

--- apps/reflect.net/components/GetStarted/GetStarted.tsx
+++ apps/reflect.net/components/GetStarted/GetStarted.tsx
@@ -1,21 +1,10 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import styles from './GetStarted.module.css';
-import stopwatchGif from '../../public/img/scaffolding-stopwatch-quickedit.gif';
 
 export function GetStarted() {
   return (
     <div className={styles.getStarted}>
       <p>Build your first multiplayer app in under a minute:</p>
-      <div className={styles.imageContainer}>
-        <Link href="https://hello.reflect.net/scaffold">
-          <Image
-            src={stopwatchGif}
-            alt={`Run Reflect in 41 seconds`}
-            className={styles.innerImage}
-          />
-        </Link>
-      </div>
       <div className={styles.ctaWrap}>
         <Link
           href="https://hello.reflect.net/scaffold"
