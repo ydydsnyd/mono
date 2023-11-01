71d6e8d4d43bd4f8badb36e3dbff1f031b5251e2
Greg Baker <greg@roci.dev>
2023-10-31 19:06:09 -0700

    feat(reflect.net): open get-location to cors requests (#1182)

--- apps/reflect.net/pages/api/get-location.ts
+++ apps/reflect.net/pages/api/get-location.ts
@@ -1,24 +1,24 @@
 import {NextRequest, NextResponse} from 'next/server';
+import {cors} from '../../api-utils/cors.js';
 
 export const config = {
   runtime: 'edge',
 };
 
-function foo(req: NextRequest) {
+export default function getLocation(req: NextRequest) {
   const country = (req.headers.get('x-vercel-ip-country') as string) ?? '';
   const city = (req.headers.get('x-vercel-ip-city') as string) ?? '';
   const region =
     (req.headers.get('x-vercel-ip-country-region') as string) ?? '';
 
-  if (!country || !city || !region) {
-    return NextResponse.json(null);
-  }
+  const result =
+    !country || !city || !region
+      ? null
+      : {
+          country,
+          city,
+          region,
+        };
 
-  return NextResponse.json({
-    country,
-    city,
-    region,
-  });
+  return cors(req, NextResponse.json(result));
 }
-
-export {foo as default};
