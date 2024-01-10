0cc9fac31380c3f9b646975486d1db77fe69bf4a
Erik Arvidsson <arv@roci.dev>
2024-01-10 16:21:49 +0100

    chore(replicache): No EventTarget in noop broadcast channel. (#1350)
    We used to `extends EventTarget` in the noop broadcast channel, but
    that's not available in React Native. Instead, we just use a plain
    object and add the `addEventListener` and `dispatchEvent` methods
    ourselves.

--- packages/replicache/src/broadcast-channel.ts
+++ packages/replicache/src/broadcast-channel.ts
@@ -1,24 +1,36 @@
-class NoopBroadcastChannel extends EventTarget implements BroadcastChannel {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import {notImplemented} from 'shared/src/asserts.js';
+
+class NoopBroadcastChannel implements BroadcastChannel {
   readonly name: string;
 
-  onmessage:
-    | ((this: BroadcastChannel, ev: MessageEvent<unknown>) => unknown)
-    | null = null;
+  onmessage: ((this: BroadcastChannel, ev: MessageEvent<any>) => any) | null =
+    null;
 
   onmessageerror:
-    | ((this: BroadcastChannel, ev: MessageEvent<unknown>) => unknown)
+    | ((this: BroadcastChannel, ev: MessageEvent<any>) => any)
     | null = null;
 
   constructor(name: string) {
-    super();
     this.name = name;
+  }
+
+  addEventListener(): void {
+    notImplemented();
+  }
+  removeEventListener(): void {
+    notImplemented();
+  }
+  dispatchEvent(): boolean {
+    notImplemented();
   }
 
   close(): void {
     // noop
   }
 
-  postMessage(_message: unknown): void {
+  postMessage(_message: any): void {
     // noop
   }
 }
