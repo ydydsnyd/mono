d8081bac9397b26b8138379c0250f8db944dde2b
Alexander King <alexhking@gmail.com>
2024-02-29 13:15:16 -0800

    [GA] Adding conversion event to hello reflect (#1460)
    * Adding conversion event to hello reflect

--- apps/reflect.net/components/GetStarted/GetStarted.tsx
+++ apps/reflect.net/components/GetStarted/GetStarted.tsx
@@ -1,7 +1,16 @@
 import Link from 'next/link';
 import styles from './GetStarted.module.css';
+import {event} from 'nextjs-google-analytics';
 
 export function GetStarted() {
+  const trackClick = () => {
+    event('Hello Reflect Clicked', {
+      action: 'click',
+      category: 'CTA Buttons',
+      label: 'Hello Reflect Link',
+    });
+  };
+
   return (
     <div className={styles.getStarted}>
       <p>Build your first multiplayer app in under a minute:</p>
@@ -9,6 +18,7 @@ export function GetStarted() {
         <Link
           href="https://hello.reflect.net/start/scaffold"
           className={styles.buttonPrimary}
+          onClick={trackClick}
         >
           Hello, Reflect
         </Link>
