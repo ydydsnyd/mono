2c638dffec02e044a2c5fa2b81b3e3667665b9cd
Erik Arvidsson <arv@roci.dev>
2024-04-08 15:39:49 +0200

    fix: Call return in mergeAsyncIterables if present
    Sometimes iterator do cleanup work in `return`. This change ensures that
    `return` is called on the iterators used in `mergeAsyncIterables`.
    
    https://discord.com/channels/830183651022471199/830183651022471202/1225550392201449482
    
    Use finally which gets executed when generators are "closed".

--- packages/replicache/src/merge-async-iterables.ts
+++ packages/replicache/src/merge-async-iterables.ts
@@ -21,36 +21,46 @@ export async function* mergeAsyncIterables<A, B>(
   const a = getIterator(iterableBase);
   const b = getIterator(iterableOverlay);
 
-  let iterResultA = await a.next();
-  let iterResultB = await b.next();
+  try {
+    let iterResultA = await a.next();
+    let iterResultB = await b.next();
 
-  while (true) {
-    if (iterResultA.done) {
-      if (iterResultB.done) {
-        return;
+    while (true) {
+      if (iterResultA.done) {
+        if (iterResultB.done) {
+          return;
+        }
+        yield iterResultB.value;
+        iterResultB = await b.next();
+        continue;
       }
-      yield iterResultB.value;
-      iterResultB = await b.next();
-      continue;
-    }
 
-    if (iterResultB.done) {
-      yield iterResultA.value;
-      iterResultA = await a.next();
-      continue;
-    }
+      if (iterResultB.done) {
+        yield iterResultA.value;
+        iterResultA = await a.next();
+        continue;
+      }
 
-    const ord = compare(iterResultA.value, iterResultB.value);
-    if (ord === 0) {
-      yield iterResultB.value;
-      iterResultA = await a.next();
-      iterResultB = await b.next();
-    } else if (ord < 0) {
-      yield iterResultA.value;
-      iterResultA = await a.next();
-    } else {
-      yield iterResultB.value;
-      iterResultB = await b.next();
+      const ord = compare(iterResultA.value, iterResultB.value);
+      if (ord === 0) {
+        yield iterResultB.value;
+        iterResultA = await a.next();
+        iterResultB = await b.next();
+      } else if (ord < 0) {
+        yield iterResultA.value;
+        iterResultA = await a.next();
+      } else {
+        yield iterResultB.value;
+        iterResultB = await b.next();
+      }
+    }
+  } finally {
+    // Both done. Call return if defined.
+    if (a.return) {
+      await a.return();
+    }
+    if (b.return) {
+      await b.return();
     }
   }
 }
