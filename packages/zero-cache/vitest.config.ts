0a8020b739e0cfc0353e37cf20e28569d730ce2b
Darick Tong <d@roci.dev>
2024-04-17 14:50:13 -0700

    workarounds needed for CI

--- packages/zero-cache/vitest.config.ts
+++ packages/zero-cache/vitest.config.ts
@@ -1,6 +1,11 @@
 import {defineWorkersConfig} from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
+  // https://github.com/vitest-dev/vitest/issues/5332#issuecomment-1977785593
+  optimizeDeps: {
+    include: ['vitest > @vitest/expect > chai'],
+    exclude: ['wa-sqlite'],
+  },
   test: {
     include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     poolOptions: {
