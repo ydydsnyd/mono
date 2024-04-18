ca0521a8bdb98b35f7dcd34c05c206776f0b2542
Darick Tong <d@roci.dev>
2024-04-17 18:04:49 -0700

    remove workarounds, as they are only relevant for @vitest/browser (and do not fix CI failures)

--- packages/zero-cache/vitest-all.config.ts
+++ packages/zero-cache/vitest-all.config.ts
@@ -1,11 +1,6 @@
 import {defineWorkersConfig} from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
-  // https://github.com/vitest-dev/vitest/issues/5332#issuecomment-1977785593
-  optimizeDeps: {
-    include: ['vitest > @vitest/expect > chai'],
-    exclude: ['wa-sqlite'],
-  },
   test: {
     include: ['src/**/*.{test,pg\\-test}.?(c|m)[jt]s?(x)'],
     poolOptions: {
--- packages/zero-cache/vitest-pg.config.ts
+++ packages/zero-cache/vitest-pg.config.ts
@@ -1,11 +1,6 @@
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-  // https://github.com/vitest-dev/vitest/issues/5332#issuecomment-1977785593
-  optimizeDeps: {
-    include: ['vitest > @vitest/expect > chai'],
-    exclude: ['wa-sqlite'],
-  },
   test: {
     include: ['src/**/*.pg\\-test.?(c|m)[jt]s?(x)'],
   },
