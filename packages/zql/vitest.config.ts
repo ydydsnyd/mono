34bc1007345d075374fd3a7b20f64765b68bb02b
Erik Arvidsson <arv@roci.dev>
2024-04-25 09:58:25 +0200

    chore(zql): Give the tests more time to run (#1636)

--- packages/zql/vitest.config.ts
+++ packages/zql/vitest.config.ts
@@ -1,1 +1,12 @@
-export {config as default} from '../shared/src/tool/vitest-config.js';
+import {defineConfig, mergeConfig} from 'vitest/config';
+
+import {config} from '../shared/src/tool/vitest-config.js';
+
+export default mergeConfig(
+  config,
+  defineConfig({
+    test: {
+      testTimeout: 20_000,
+    },
+  }),
+);
