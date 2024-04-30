f9755e2de6ee8ffd50072dcd07d7d6e986272727
Matt <1009003+tantaman@users.noreply.github.com>
2024-04-30 14:07:00 -0700

    chore(zql) re-enalbe previously broken integration tests for distinct

--- packages/zero-client/src/client/zql/distinct-integration.test.ts
+++ packages/zero-client/src/client/zql/distinct-integration.test.ts
@@ -1,4 +1,5 @@
 import {describe, expect, test} from 'vitest';
+import {joinSymbol} from '../../../../zql/src/zql/ivm/types.js';
 import {newZero, Track, TrackArtist} from './integration-test-util.js';
 
 describe('distinct', async () => {
@@ -92,31 +93,72 @@ describe('distinct', async () => {
       zql: z.query.track.distinct('track.albumId'),
       expected: [tracks[0]],
     },
-    // {
-    //   test: 'distinct after 1:many join',
-    //   zql: z.query.track
-    //     .join(
-    //       z.query.trackArtist,
-    //       'trackArtist',
-    //       'track.id',
-    //       'trackArtist.trackId',
-    //     )
-    //     .distinct('track.id'),
-    //   expected: tracks,
-    // },
-    // {
-    //   test: 'distinct after 1:many join thru junction edge',
-    //   zql: z.query.track
-    //     .join(
-    //       z.query.trackArtist,
-    //       'trackArtist',
-    //       'track.id',
-    //       'trackArtist.trackId',
-    //     )
-    //     .join(z.query.artist, 'artist', 'trackArtist.artistId', 'artist.id')
-    //     .distinct('track.id'),
-    //   expected: tracks,
-    // },
+    {
+      test: 'distinct after 1:many join',
+      zql: z.query.track
+        .join(
+          z.query.trackArtist,
+          'trackArtist',
+          'track.id',
+          'trackArtist.trackId',
+        )
+        .distinct('track.id'),
+      expected: [
+        {
+          id: '1-2_2',
+          track: {id: '2', length: 200, title: 'b', albumId: '1'},
+          trackArtist: {id: '1-2', artistId: '1', trackId: '2'},
+          [joinSymbol]: true,
+        },
+        {
+          id: '1-3_3',
+          track: {id: '3', length: 300, title: 'c', albumId: '1'},
+          trackArtist: {id: '1-3', artistId: '1', trackId: '3'},
+          [joinSymbol]: true,
+        },
+        {
+          id: '1_1-1',
+          track: {id: '1', length: 100, title: 'a', albumId: '1'},
+          trackArtist: {id: '1-1', artistId: '1', trackId: '1'},
+          [joinSymbol]: true,
+        },
+      ],
+    },
+    {
+      test: 'distinct after 1:many join thru junction edge',
+      zql: z.query.track
+        .join(
+          z.query.trackArtist,
+          'trackArtist',
+          'track.id',
+          'trackArtist.trackId',
+        )
+        .join(z.query.artist, 'artist', 'trackArtist.artistId', 'artist.id')
+        .distinct('track.id'),
+      expected: [
+        {
+          id: '1_1-2_2',
+          track: {id: '2', length: 200, title: 'b', albumId: '1'},
+          trackArtist: {id: '1-2', artistId: '1', trackId: '2'},
+          artist: {id: '1', name: 'a'},
+          [joinSymbol]: true,
+        },
+        {
+          id: '1_1-3_3',
+          track: {id: '3', length: 300, title: 'c', albumId: '1'},
+          trackArtist: {id: '1-3', artistId: '1', trackId: '3'},
+          artist: {id: '1', name: 'a'},
+          [joinSymbol]: true,
+        },
+        {
+          id: '1_1_1-1',
+          track: {id: '1', length: 100, title: 'a', albumId: '1'},
+          trackArtist: {id: '1-1', artistId: '1', trackId: '1'},
+          artist: {id: '1', name: 'a'},
+          [joinSymbol]: true,
+        },
+      ],
+    },
   ])('$test', async ({zql, expected}) => {
     const stmt = zql.prepare();
     const rows = await stmt.exec();
