cd1deb0a56dddd0536b57248a54d6daaba4128e1
Matt <1009003+tantaman@users.noreply.github.com>
2024-05-10 17:02:10 -0400

    chore(zql): more tests for group-by, limited and ordered joins

--- packages/zero-client/src/client/zql/order-limit-integration.test.ts
+++ packages/zero-client/src/client/zql/order-limit-integration.test.ts
@@ -26,7 +26,7 @@ import {
 } from './integration-test-util.js';
 import {makeComparator} from '@rocicorp/zql/src/zql/query/statement.js';
 import {must} from '../../../../shared/src/must.js';
-import {joinSymbol} from '@rocicorp/zql/src/zql/ivm/types.js';
+import {Comparator, joinSymbol} from '@rocicorp/zql/src/zql/ivm/types.js';
 
 describe('sorting and limiting with different query operations', async () => {
   const z = newZero();
@@ -91,8 +91,8 @@ describe('sorting and limiting with different query operations', async () => {
     });
   };
 
-  const groupTracksByAlbum = () => {
-    const grouped = tracks.sort(canonicalComparator).reduce(
+  const groupTracksByAlbum = (comp: Comparator<Track>) => {
+    const grouped = tracks.sort(comp).reduce(
       (acc, t) => {
         acc[t.albumId] = acc[t.albumId] || [];
         acc[t.albumId].push(t);
@@ -104,7 +104,7 @@ describe('sorting and limiting with different query operations', async () => {
     for (const key of Object.keys(grouped)) {
       ret.push(grouped[key][0]);
     }
-    return ret.sort(canonicalComparator);
+    return ret.sort(comp);
   };
 
   test.each([
@@ -185,9 +185,6 @@ describe('sorting and limiting with different query operations', async () => {
         return albums.map(joinAlbumToArtist).sort(c).slice(0, 10);
       },
     },
-    // TODO: test that join removes reply
-    // TODO: test that source does not create new sources for:
-    // - same order, desc/asc change, undefined order
     {
       name: '3-way join, no limits, ordered, asc',
       query: () =>
@@ -212,33 +209,59 @@ describe('sorting and limiting with different query operations', async () => {
         return tracks.flatMap(joinTrackToArtists).sort(c);
       },
     },
-    // check logs to ensure we're not creating new sources
     {
       name: 'group-by, no limit',
       query: () => z.query.track.groupBy('track.albumId'),
-      expected: () => groupTracksByAlbum(),
+      expected: () => groupTracksByAlbum(canonicalComparator),
     },
     {
       name: 'group-by, limit 10',
       query: () => z.query.track.groupBy('track.albumId').limit(10),
-      expected: () => groupTracksByAlbum().slice(0, 10),
+      expected: () => groupTracksByAlbum(canonicalComparator).slice(0, 10),
     },
     {
       name: 'group-by limit 10 desc',
       query: () =>
         z.query.track.groupBy('track.albumId').desc('track.title').limit(10),
       expected: () =>
-        groupTracksByAlbum()
+        groupTracksByAlbum(makeComparator(['title', 'id'], 'desc'))
           .sort(makeComparator(['title', 'id'], 'desc'))
           .slice(0, 10),
     },
-    // 3 way join limited
+    {
+      name: '3-way join, limits, ordered, asc',
+      query: () =>
+        z.query.track
+          .join(z.query.trackArtist, 'trackArtist', 'id', 'trackId')
+          .join(z.query.artist, 'artist', 'trackArtist.artistId', 'id')
+          .limit(10)
+          .asc('track.title', 'artist.name'),
+      expected: () => {
+        const c = makeComparator(['track.title', 'artist.name', 'id'], 'asc');
+        return tracks.flatMap(joinTrackToArtists).sort(c).slice(0, 10);
+      },
+    },
+    {
+      name: '3-way join, limits, ordered, desc',
+      query: () =>
+        z.query.track
+          .join(z.query.trackArtist, 'trackArtist', 'id', 'trackId')
+          .join(z.query.artist, 'artist', 'trackArtist.artistId', 'id')
+          .desc('track.title', 'artist.name')
+          .limit(10),
+      expected: () => {
+        const c = makeComparator(['track.title', 'artist.name', 'id'], 'desc');
+        return tracks.flatMap(joinTrackToArtists).sort(c).slice(0, 10);
+      },
+    },
     // 3 way join unlimited w/ group-by and aggregation against group
-    // TODO(mlaw): test group-by
     // TODO(mlaw): test full table aggregations
     // TODO(mlaw): test group-by with aggregation
     // TODO(mlaw): test left join for same cases as join
-    // TODO(mlaw): removing things to cause us to come under the limit re-fills the window
+    // TODO(mlaw): test removing things to cause us to come under the limit re-fills the window
+    // ^ this should be a unit test against the view.
+    // TODO(mlaw): test adding thins maintains the window correctly. E.g., `desc` logic is probably wrong rn.
+    // ^ this should be a unit test against the view.
   ])('$name', async ({query, expected}) => {
     const stmt = query().prepare();
     const rows = await stmt.exec();
