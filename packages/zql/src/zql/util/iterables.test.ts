e13178c833b9f05f4a1d76c69c00b3dd67d9bf14
Matt <1009003+tantaman@users.noreply.github.com>
2024-05-13 21:32:14 -0400

    feat(zql): allow callers of `genFlatMap/Map/Filter` to pass a `finally` handler
    This is to allow operators to do bookkeeping when a downstream stops iterating early due to a limit.

--- packages/zql/src/zql/util/iterables.test.ts
+++ packages/zql/src/zql/util/iterables.test.ts
@@ -1,5 +1,5 @@
 import {expect, test} from 'vitest';
-import {genFlatMap, mapIter} from './iterables.js';
+import {genFilter, genFlatMap, genMap, mapIter} from './iterables.js';
 
 test('mapIter', () => {
   const iterable = [1, 2, 3];
@@ -17,4 +17,85 @@ test('genFlatMap', () => {
   expect([...flatMapper]).toEqual([1, 2, 3, 4, 5, 6]);
   // can iterate it a second time
   expect([...flatMapper]).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test('genMap finally handling', () => {
+  // iterate manually
+  // iterate with a loop construct
+
+  const iterable = [1, 2, 3];
+  let finallyCalled = false;
+  const mappedIterable = genMap(
+    iterable,
+    x => x + 1,
+    () => {
+      finallyCalled = true;
+    },
+  );
+
+  expect(finallyCalled).toBe(false);
+
+  for (const _ of mappedIterable) {
+    // do nothing
+  }
+
+  expect(finallyCalled).toBe(true);
+
+  finallyCalled = false;
+  const manualIter = mappedIterable[Symbol.iterator]();
+  manualIter.next();
+  manualIter.return();
+  expect(finallyCalled).toBe(true);
+});
+
+test('genFilter finally handling', () => {
+  const iterable = [1, 2, 3];
+  let finallyCalled = false;
+  const filteredIterable = genFilter(
+    iterable,
+    x => x > 1,
+    () => {
+      finallyCalled = true;
+    },
+  );
+
+  expect(finallyCalled).toBe(false);
+
+  for (const _ of filteredIterable) {
+    // do nothing
+  }
+
+  expect(finallyCalled).toBe(true);
+
+  finallyCalled = false;
+  const manualIter = filteredIterable[Symbol.iterator]();
+  manualIter.next();
+  manualIter.return();
+  expect(finallyCalled).toBe(true);
+});
+
+test('genFlatMap finally handling', () => {
+  const iterable = [[1], [2, 3], [4, 5, 6]];
+  let finallyCalled = false;
+  const flatMapper = genFlatMap(
+    () => iterable,
+    x => x,
+    () => {
+      finallyCalled = true;
+    },
+  );
+
+  expect(finallyCalled).toBe(false);
+
+  for (const _ of flatMapper) {
+    // do nothing
+  }
+
+  expect(finallyCalled).toBe(true);
+
+  finallyCalled = false;
+  const manualIter = flatMapper[Symbol.iterator]();
+  manualIter.next();
+  manualIter.return();
+  expect(finallyCalled).toBe(true);
 });
--- packages/zql/src/zql/util/iterables.ts
+++ packages/zql/src/zql/util/iterables.ts
@@ -1,8 +1,18 @@
-export function genMap<T, U>(s: Iterable<T>, cb: (x: T) => U) {
+export function genMap<T, U>(
+  s: Iterable<T>,
+  cb: (x: T) => U,
+  finallyCb?: () => void | undefined,
+) {
   return {
     *[Symbol.iterator]() {
-      for (const x of s) {
-        yield cb(x);
+      try {
+        for (const x of s) {
+          yield cb(x);
+        }
+      } finally {
+        if (finallyCb !== undefined) {
+          finallyCb();
+        }
       }
     },
   };
@@ -11,26 +21,61 @@ export function genMap<T, U>(s: Iterable<T>, cb: (x: T) => U) {
 export function genFilter<S extends T, T>(
   s: Iterable<T>,
   f: (x: T) => x is S,
+  finallyCb?: () => void | undefined,
 ): {
   [Symbol.iterator](): Generator<S, void, unknown>;
 };
 export function genFilter<T>(
   s: Iterable<T>,
   f: (x: T) => boolean,
+  finallyCb?: () => void | undefined,
 ): {
   [Symbol.iterator](): Generator<T, void, unknown>;
 };
 export function genFilter<S extends T, T>(
   s: Iterable<T>,
   cb: (x: T) => boolean,
+  finallyCb?: () => void | undefined,
 ): {
   [Symbol.iterator](): Generator<S, void, unknown>;
 } {
   return {
     *[Symbol.iterator]() {
-      for (const x of s) {
-        if (cb(x)) {
-          yield x as S;
+      try {
+        for (const x of s) {
+          if (cb(x)) {
+            yield x as S;
+          }
+        }
+      } finally {
+        if (finallyCb !== undefined) {
+          finallyCb();
+        }
+      }
+    },
+  };
+}
+
+/**
+ * idk why `iter` needs to be returned from a lambda below.
+ * Something breaks in `reduce-operator` if we change this.
+ * Seems like something is wrong in `reduce` then.
+ */
+export function genFlatMap<T, U>(
+  iter: () => Iterable<T>,
+  f: (t: T, index: number) => Iterable<U>,
+  finallyCb?: () => void | undefined,
+) {
+  return {
+    *[Symbol.iterator]() {
+      try {
+        let index = 0;
+        for (const t of iter()) {
+          yield* f(t, index++);
+        }
+      } finally {
+        if (finallyCb !== undefined) {
+          finallyCb();
         }
       }
     },
@@ -45,24 +90,4 @@ export function* mapIter<T, U>(
   for (const t of iter) {
     yield f(t, index++);
   }
-}
-
-/**
- * Flat maps the items returned from the iterable.
- *
- * `iter` is a lambda that returns an iterable
- * so this function can return an `IterableIterator`
- */
-export function genFlatMap<T, U>(
-  iter: () => Iterable<T>,
-  f: (t: T, index: number) => Iterable<U>,
-) {
-  return {
-    *[Symbol.iterator]() {
-      let index = 0;
-      for (const t of iter()) {
-        yield* f(t, index++);
-      }
-    },
-  };
 }
