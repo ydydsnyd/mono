d25963d9c397208d589d2736c34ea368c2b87098
Greg Baker <greg@roci.dev>
2024-05-16 16:02:26 -0700

    refactor(zero-cache): encapsulate connect handling in connect.ts (#1844)

--- packages/zero-cache/src/services/connection.ts
+++ packages/zero-cache/src/services/connection.ts
@@ -1,6 +1,7 @@
 import type {LogContext} from '@rocicorp/logger';
 import * as valita from 'shared/src/valita.js';
 import {
+  ConnectedMessage,
   Downstream,
   ErrorKind,
   ErrorMessage,
@@ -11,6 +12,64 @@ import type {ServiceRunner} from './service-runner.js';
 import type {SyncContext, ViewSyncer} from './view-syncer/view-syncer.js';
 import type {CancelableAsyncIterable} from '../types/streams.js';
 import type {Mutagen} from './mutagen/mutagen.js';
+import {assert} from 'shared/src/asserts.js';
+
+export function handleConnection(
+  lc: LogContext,
+  serviceRunner: ServiceRunner,
+  clientConnections: Map<string, Connection>,
+  request: Request,
+): Response {
+  if (request.headers.get('Upgrade') !== 'websocket') {
+    lc.info?.('Missing Upgrade header for', request.url);
+    return new Response('expected WebSocket Upgrade header', {status: 400});
+  }
+
+  const {0: clientWS, 1: serverWS} = new WebSocketPair();
+  const url = new URL(request.url);
+  serverWS.accept();
+
+  const {params, error} = getConnectParams(url);
+
+  if (error !== null) {
+    sendError(lc, serverWS, 'InvalidConnectionRequest', error);
+  } else {
+    const {clientID} = params;
+    const existing = clientConnections.get(clientID);
+    if (existing) {
+      existing.close();
+    }
+    const connection = new Connection(
+      lc,
+      serviceRunner,
+      params,
+      serverWS,
+      () => {
+        if (clientConnections.get(clientID) === connection) {
+          clientConnections.delete(clientID);
+        }
+      },
+    );
+    clientConnections.set(clientID, connection);
+  }
+
+  // Sec-WebSocket-Protocol is used as a mechanism for sending `auth`
+  // since custom headers are not supported by the browser WebSocket API, the
+  // Sec-WebSocket-Protocol semantics must be followed. Send a
+  // Sec-WebSocket-Protocol response header with a value matching the
+  // Sec-WebSocket-Protocol request header, to indicate support for the
+  // protocol, otherwise the client will close the connection.
+  const responseHeaders = new Headers();
+  const protocol = request.headers.get('Sec-WebSocket-Protocol');
+  if (protocol) {
+    responseHeaders.set('Sec-WebSocket-Protocol', protocol);
+  }
+  return new Response(null, {
+    status: 101,
+    webSocket: clientWS,
+    headers: responseHeaders,
+  });
+}
 
 /**
  * Represents a connection between the client and server.
@@ -25,65 +84,92 @@ export class Connection {
   readonly #clientGroupID: string;
   readonly #syncContext: SyncContext;
   readonly #lc: LogContext;
+  readonly #onClose: () => void;
 
   readonly #viewSyncer: ViewSyncer;
   readonly #mutagen: Mutagen;
 
   #outboundStream: CancelableAsyncIterable<Downstream> | undefined;
+  #closed = false;
 
   constructor(
     lc: LogContext,
     serviceRunner: ServiceRunner,
-    clientGroupID: string,
-    clientID: string,
-    wsID: string,
-    baseCookie: string | null,
+    connectParams: ConnectParams,
     ws: WebSocket,
+    onClose: () => void,
   ) {
     this.#ws = ws;
+    const {clientGroupID, clientID, wsID, baseCookie} = connectParams;
     this.#clientGroupID = clientGroupID;
     this.#syncContext = {clientID, wsID, baseCookie};
     this.#lc = lc
       .withContext('clientID', clientID)
       .withContext('clientGroupID', clientGroupID)
       .withContext('wsID', wsID);
+    this.#onClose = onClose;
 
     this.#viewSyncer = serviceRunner.getViewSyncer(clientGroupID);
     this.#mutagen = serviceRunner.getMutagen(clientGroupID);
 
-    this.#ws.addEventListener('message', this.#onMessage);
+    this.#ws.addEventListener('message', this.#handleMessage);
+    this.#ws.addEventListener('close', this.#handleClose);
+    this.#ws.addEventListener('error', this.#handleError);
+
+    const connectedMessage: ConnectedMessage = [
+      'connected',
+      {wsid: wsID, timestamp: Date.now()},
+    ];
+    send(ws, connectedMessage);
   }
 
   close() {
-    this.#ws.close();
+    if (this.#closed) {
+      return;
+    }
+    this.#closed = true;
+    this.#ws.removeEventListener('message', this.#handleMessage);
+    this.#ws.removeEventListener('close', this.#handleClose);
+    this.#ws.removeEventListener('error', this.#handleError);
     this.#outboundStream?.cancel();
     this.#outboundStream = undefined;
+    this.#onClose();
+    if (this.#ws.readyState !== WebSocket.READY_STATE_CLOSED) {
+      this.#ws.close();
+    }
+
+    // spin down services if we have
+    // no more client connections for the client group?
   }
 
-  #onMessage = async (event: MessageEvent) => {
+  #handleMessage = async (event: MessageEvent) => {
     const lc = this.#lc;
     const data = event.data.toString();
-    const ws = this.#ws;
     const viewSyncer = this.#viewSyncer;
+    if (this.#closed) {
+      this.#lc.debug?.('Ignoring message received after closed', data);
+      return;
+    }
 
     let msg;
     try {
       const value = JSON.parse(data);
       msg = valita.parse(value, upstreamSchema);
     } catch (e) {
-      closeWithError(lc, ws, 'InvalidMessage', String(e));
+      this.#closeWithError('InvalidMessage', String(e));
       return;
     }
     try {
       const msgType = msg[0];
       switch (msgType) {
         case 'ping':
-          handlePing(ws);
+          this.send(['pong', {}] satisfies PongMessage);
           break;
         case 'push': {
           const {clientGroupID, mutations} = msg[1];
           if (clientGroupID !== this.#clientGroupID) {
-            throw new Error(
+            this.#closeWithError(
+              'InvalidPush',
               `clientGroupID in mutation "${clientGroupID}" does not match ` +
                 `clientGroupID of connection "${this.#clientGroupID}`,
             );
@@ -91,7 +177,7 @@ export class Connection {
           for (const mutation of mutations) {
             const errorDesc = await this.#mutagen.processMutation(mutation);
             if (errorDesc !== undefined) {
-              sendError(lc, ws, 'MutationFailed', errorDesc);
+              this.sendError('MutationFailed', errorDesc);
             }
           }
           break;
@@ -111,7 +197,7 @@ export class Connection {
             msg,
           );
 
-          void this.proxyOutbound(lc, ws, this.#outboundStream);
+          void this.#proxyOutbound(this.#outboundStream);
           break;
         }
         default:
@@ -119,27 +205,52 @@ export class Connection {
       }
     } catch (e) {
       // TODO: Determine the ErrorKind from a custom Error type for e.
-      closeWithError(lc, ws, 'InvalidMessage', String(e));
-      this.close();
+      this.#closeWithError('InvalidMessage', String(e));
     }
   };
 
-  async proxyOutbound(
-    lc: LogContext,
-    ws: WebSocket,
-    outboundStream: CancelableAsyncIterable<Downstream>,
-  ) {
+  #handleClose = (e: CloseEvent) => {
+    const {code, reason, wasClean} = e;
+    this.#lc.info?.('WebSocket close event', {code, reason, wasClean});
+    this.close();
+  };
+
+  #handleError = (e: ErrorEvent) => {
+    this.#lc.error?.('WebSocket error event', e.message, e.error);
+  };
+
+  async #proxyOutbound(outboundStream: CancelableAsyncIterable<Downstream>) {
     try {
       for await (const outMsg of outboundStream) {
-        send(ws, outMsg);
+        this.send(outMsg);
       }
-      lc.info?.('downstream closed by ViewSyncer');
+      this.#lc.info?.('downstream closed by ViewSyncer');
+      this.close();
     } catch (e) {
       // TODO: Determine the ErrorKind from a custom Error type for e.
-      closeWithError(lc, ws, 'InvalidMessage', String(e));
-    } finally {
-      this.close();
+      this.#closeWithError('InvalidMessage', String(e));
     }
+  }
+
+  #closeWithError(
+    kind: ErrorKind,
+    message = '',
+    logLevel: 'info' | 'error' = 'info',
+  ) {
+    this.sendError(kind, message, logLevel);
+    this.close();
+  }
+
+  send(data: Downstream) {
+    send(this.#ws, data);
+  }
+
+  sendError(
+    kind: ErrorKind,
+    message = '',
+    logLevel: 'info' | 'error' = 'info',
+  ) {
+    sendError(this.#lc, this.#ws, kind, message, logLevel);
   }
 }
 
@@ -162,17 +273,89 @@ export function sendError(
   send(ws, errorMessage);
 }
 
-export function closeWithError(
-  lc: LogContext,
-  ws: WebSocket,
-  kind: ErrorKind,
-  message = '',
-  logLevel: 'info' | 'error' = 'info',
-) {
-  sendError(lc, ws, kind, message, logLevel);
-}
+type ConnectParams = {
+  readonly clientID: string;
+  readonly clientGroupID: string;
+  readonly baseCookie: string | null;
+  readonly timestamp: number;
+  readonly lmID: number;
+  readonly wsID: string;
+  readonly debugPerf: boolean;
+};
 
-function handlePing(ws: WebSocket) {
-  const pongMessage: PongMessage = ['pong', {}];
-  send(ws, pongMessage);
+function getConnectParams(url: URL):
+  | {
+      params: ConnectParams;
+      error: null;
+    }
+  | {
+      params: null;
+      error: string;
+    } {
+  function getParam(name: string, required: true): string;
+  function getParam(name: string, required: boolean): string | null;
+  function getParam(name: string, required: boolean) {
+    const value = url.searchParams.get(name);
+    if (value === '' || value === null) {
+      if (required) {
+        throw new Error(`invalid querystring - missing ${name}`);
+      }
+      return null;
+    }
+    return value;
+  }
+
+  function getIntegerParam(name: string, required: true): number;
+  function getIntegerParam(name: string, required: boolean): number | null;
+  function getIntegerParam(name: string, required: boolean) {
+    const value = getParam(name, required);
+    if (value === null) {
+      return null;
+    }
+    const int = parseInt(value);
+    if (isNaN(int)) {
+      throw new Error(
+        `invalid querystring parameter ${name}, got: ${value}, url: ${url}`,
+      );
+    }
+    return int;
+  }
+
+  function getBooleanParam(name: string): boolean {
+    const value = getParam(name, false);
+    if (value === null) {
+      return false;
+    }
+    return value === 'true';
+  }
+
+  try {
+    const clientID = getParam('clientID', true);
+    const clientGroupID = getParam('clientGroupID', true);
+    const baseCookie = getParam('baseCookie', false);
+    const timestamp = getIntegerParam('ts', true);
+    const lmID = getIntegerParam('lmid', true);
+    const wsID = getParam('wsid', false) ?? '';
+    const debugPerf = getBooleanParam('debugPerf');
+
+    return {
+      params: {
+        clientID,
+        clientGroupID,
+        baseCookie,
+        timestamp,
+        lmID,
+        wsID,
+        debugPerf,
+      },
+      error: null,
+    };
+  } catch (e) {
+    assert(e instanceof Error);
+
+    return {
+      params: null,
+      error: e.message,
+    };
+  }
 }
--- packages/zero-cache/src/services/runner-do.ts
+++ packages/zero-cache/src/services/runner-do.ts
@@ -1,9 +1,6 @@
 import {LogContext, LogLevel, LogSink} from '@rocicorp/logger';
 import {BaseContext, Router} from 'cf-shared/src/router.js';
-import type {ConnectedMessage} from 'zero-protocol';
-import type {ErrorKind} from 'zero-protocol/src/error.js';
-import {getConnectRequest} from '../connect.js';
-import {Connection, closeWithError, send} from './connection.js';
+import {Connection, handleConnection} from './connection.js';
 import {CONNECT_URL_PATTERN, STATUS_URL_PATTERN} from './paths.js';
 import {ServiceRunner, ServiceRunnerEnv} from './service-runner.js';
 
@@ -11,7 +8,7 @@ export class ServiceRunnerDO {
   readonly #lc: LogContext;
   readonly #serviceRunner: ServiceRunner;
   readonly #router = new Router();
-  readonly #clients = new Map<string, Connection>();
+  readonly #clientConnections = new Map<string, Connection>();
 
   constructor(
     logSink: LogSink,
@@ -39,81 +36,13 @@ export class ServiceRunnerDO {
     this.#router.register(STATUS_URL_PATTERN, this.#status);
   }
 
-  #connect = async (_ctx: BaseContext, request: Request): Promise<Response> => {
-    if (request.headers.get('Upgrade') !== 'websocket') {
-      this.#lc.info?.('missing Upgrade header for', request.url);
-      return new Response('expected websocket Upgrade header', {status: 400});
-    }
-
-    const {0: clientWS, 1: serverWS} = new WebSocketPair();
-    const url = new URL(request.url);
-    serverWS.accept();
-
-    await this.#handleConnection(serverWS, url);
-
-    // Sec-WebSocket-Protocol is being used as a mechanism for sending `auth`
-    // since custom headers are not supported by the browser WebSocket API, the
-    // Sec-WebSocket-Protocol semantics must be followed. Send a
-    // Sec-WebSocket-Protocol response header with a value matching the
-    // Sec-WebSocket-Protocol request header, to indicate support for the
-    // protocol, otherwise the client will close the connection.
-    const responseHeaders = new Headers();
-    const protocol = request.headers.get('Sec-WebSocket-Protocol');
-    if (protocol) {
-      responseHeaders.set('Sec-WebSocket-Protocol', protocol);
-    }
-    return new Response(null, {
-      status: 101,
-      webSocket: clientWS,
-      headers: responseHeaders,
-    });
-  };
-
-  // eslint-disable-next-line require-await
-  #handleConnection = async (serverWS: WebSocket, url: URL) => {
-    const closeWithErrorLocal = (ek: ErrorKind, msg: string) => {
-      closeWithError(this.#lc, serverWS, ek, msg);
-    };
-
-    const {result, error} = getConnectRequest(url);
-    if (error !== null) {
-      closeWithErrorLocal('InvalidConnectionRequest', error);
-      return;
-    }
-    const {clientGroupID, clientID, baseCookie, wsid} = result;
-
-    const existing = this.#clients.get(clientID);
-    if (existing) {
-      this.#lc.info?.('closing old socket');
-      existing.close();
-    }
-
-    const connection = new Connection(
-      this.#lc,
+  #connect = (ctx: BaseContext, request: Request): Response =>
+    handleConnection(
+      ctx.lc,
       this.#serviceRunner,
-      clientGroupID,
-      clientID,
-      wsid,
-      baseCookie,
-      serverWS,
+      this.#clientConnections,
+      request,
     );
-    this.#clients.set(clientID, connection);
-
-    serverWS.addEventListener('close', () => {
-      // spin down services if we have
-      // no more client connections for the client group?
-      this.#lc.info?.('Connection closed', {clientID, wsid});
-    });
-    serverWS.addEventListener('error', e => {
-      this.#lc.error?.('Unhandled error in ws connection', e);
-    });
-
-    const connectedMessage: ConnectedMessage = [
-      'connected',
-      {wsid, timestamp: Date.now()},
-    ];
-    send(serverWS, connectedMessage);
-  };
 
   #status = async (_ctx: BaseContext, _request: Request): Promise<Response> => {
     const status = await this.#serviceRunner.status();
