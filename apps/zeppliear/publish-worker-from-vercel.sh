ea18702011c768d3f8f9a528f8151f9267386795
Cesar Alaestante <cesar@roci.dev>
2024-05-24 09:12:50 -0700

    fix(zeppliear): fix publish worker sandbox (#1924)

--- apps/zeppliear/publish-worker-from-vercel.sh
+++ apps/zeppliear/publish-worker-from-vercel.sh
@@ -3,4 +3,8 @@ if [ "$VERCEL_PRODUCTION_BUILD" ]
 then
   echo "Production build pushing prod worker"
   npm run publish-worker-prod
+elif [ "$VERCEL_SANDBOX_BUILD" ]
+then
+  echo "Sandbox build pushing sandbox worker"
+  npm run publish-worker-sandbox
 fi
