846f715da0b4c08bd94e73eaea54486a775b9f9f
Matthew Wonlaw <mlaw@roci.dev>
2024-06-10 13:06:10 -0400

    playground to profile zql

---
+++ apps/playground/src/App.tsx
@@ -0,0 +1,10 @@
+import {TestContext} from '@rocicorp/zql/src/context/test-context.js';
+import './App.css';
+
+function App() {
+  function runZQL() {}
+
+  return <div onClick={runZQL}>Run ZQL</div>;
+}
+
+export default App;
