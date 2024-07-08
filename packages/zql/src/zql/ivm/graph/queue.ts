715a5fbc78a2e09e75a53303726fde4be7bf6071
Aaron Boodman <aaron@aaronboodman.com>
2024-07-08 01:12:40 -1000

    chore(zql): Remove unused filed queue.ts.

--- packages/zql/src/zql/ivm/graph/queue.ts
+++
@@ -1,7 +0,0 @@
-import type {Multiset} from '../multiset.js';
-import type {Version} from '../types.js';
-import type {Reply} from './message.js';
-
-export type QueueEntry<T> =
-  | readonly [version: Version, multiset: Multiset<T>, reply: Reply]
-  | readonly [version: Version, multiset: Multiset<T>];
