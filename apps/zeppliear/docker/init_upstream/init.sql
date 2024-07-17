55d88d58d59bdd2b40aa0e51e5e54f25387fff95
Matthew Wonlaw <mlaw@roci.dev>
2024-07-17 09:10:01 -0400

    chore(zeppliear): create indices on the upstream
    I think that it makes the most sense to replicate indices from upstream to downstream to start. This makes it so indices are only need to be managed in a single place by users and, saves us some steps setting up demo apps.

--- apps/zeppliear/docker/init_upstream/init.sql
+++ apps/zeppliear/docker/init_upstream/init.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS "member", "issue", "comment", "label", "issueLabel" CASCADE;
-VACUUM;
+
 CREATE TABLE member (
     "id" VARCHAR PRIMARY KEY,
     "name" VARCHAR NOT NULL
@@ -55,3 +55,22 @@ COPY "comment" FROM PROGRAM 'gzip -cd /docker-entrypoint-initdb.d/comments_3.csv
 COPY "comment" FROM PROGRAM 'gzip -cd /docker-entrypoint-initdb.d/comments_4.csv.gz' WITH CSV HEADER;
 COPY "comment" FROM PROGRAM 'gzip -cd /docker-entrypoint-initdb.d/comments_5.csv.gz' WITH CSV HEADER;
 COPY "comment" FROM PROGRAM 'gzip -cd /docker-entrypoint-initdb.d/comments_6.csv.gz' WITH CSV HEADER;
+
+-- Create the indices on upstream so we can copy to downstream on replication.
+-- We have discussed that, in the future, the indices of the Zero replica
+-- can / should diverge from the indices of the upstream. This is because
+-- the Zero replica could be serving a different set of applications than the
+-- upstream. If that is true, it would be beneficial to have indices dedicated
+-- to those use cases. This may not be true, however.
+--
+-- Until then, I think it makes the most sense to copy the indices from upstream
+-- to the replica. The argument in favor of this is that it gives the user a single
+-- place to manage indices and it saves us a step in setting up our demo apps.
+CREATE INDEX issuelabel_issueid_idx ON "issueLabel" ("issueID");
+CREATE INDEX issue_modified_idx ON issue (modified);
+CREATE INDEX issue_created_idx ON issue (created);
+CREATE INDEX issue_priority_modified_idx ON issue (priority,modified);
+CREATE INDEX issue_status_modified_idx ON issue (status,modified);
+CREATE INDEX comment_issueid_idx ON "comment" ("issueID");
+
+VACUUM;
