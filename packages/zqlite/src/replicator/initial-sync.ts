a3afb86b7c53ee14bde1072737b9bd73263a1639
Matthew Wonlaw <mlaw@roci.dev>
2024-07-17 12:36:05 -0400

    fix(zqlite): create the `zero` schema on `postgres` upstream

--- packages/zqlite/src/replicator/initial-sync.ts
+++ packages/zqlite/src/replicator/initial-sync.ts
@@ -178,6 +178,14 @@ export async function copy(
   lc.info?.('Schema copied');
 
   let confirmedLsn = '0/00000000';
+  await sql`CREATE SCHEMA IF NOT EXISTS zero;`;
+  await sql`CREATE TABLE IF NOT EXISTS zero.clients (
+      "clientGroupID"  TEXT    NOT NULL,
+      "clientID"       TEXT    NOT NULL,
+      "lastMutationID" BIGINT,
+      "userID"         TEXT,
+      PRIMARY KEY("clientGroupID", "clientID")
+    );`;
   await sql.begin('ISOLATION LEVEL REPEATABLE READ', async sql => {
     await copyDataToSQLite(lc, sql, sqliteDb);
 
