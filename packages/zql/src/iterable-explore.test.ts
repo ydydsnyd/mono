689ebd3730e22108f910c75e1be698b55ae23b99
Matthew Wonlaw <mlaw@roci.dev>
2024-07-31 19:43:57 -0400

    experiment with restartable IterableIterators

--- packages/zql/src/iterable-explore.test.ts
+++ packages/zql/src/iterable-explore.test.ts
@@ -37,12 +37,12 @@ function* sourceToStream(tableName: string, source: Iterable<Entry<unknown>>) {
 function* filter(
   path: string[],
   iterable: Iterable<ItemType>,
-  cb: (v: unknown) => boolean,
-): Iterable<ItemType> {
+  cb: (v: Entry<unknown>) => boolean,
+): IterableIterator<ItemType> {
   const [head, ...tail] = path;
   for (const row of iterable) {
     if (tail.length === 0) {
-      if (cb(row[head])) {
+      if (cb(row[head] as Entry<unknown>)) {
         yield row;
       }
     } else {
@@ -52,6 +52,33 @@ function* filter(
       } as ItemType;
     }
   }
+}
+
+function restartable<T>(generatorFunc: () => Iterator<T>) {
+  let iter: Iterator<T>;
+  return {
+    [Symbol.iterator]() {
+      iter = generatorFunc();
+      return this;
+    },
+    next() {
+      return iter.next();
+    },
+    return() {
+      return iter.return!();
+    },
+    throw(e: unknown) {
+      return iter.throw!(e);
+    },
+  };
+}
+
+function restartableFilter(
+  path: string[],
+  iterable: Iterable<ItemType>,
+  predicate: (v: Entry<unknown>) => boolean,
+) {
+  return restartable(() => filter(path, iterable, predicate));
 }
 
 test('filter against a root source', () => {
@@ -75,3 +102,57 @@ test('filter against a root source', () => {
     },
   ]);
 });
+
+test('re-pulling the stream', () => {
+  const stream = restartableFilter(
+    ['issue'],
+    restartable(() => sourceToStream('issue', issueSource)),
+    v => (v as Entry<Issue>)[0].title === 'issue 1',
+  );
+
+  const result = [...stream];
+
+  expect(result).toEqual([
+    {
+      issue: [
+        {
+          id: 1,
+          title: 'issue 1',
+        },
+        1,
+      ],
+    },
+  ]);
+
+  const result2 = [...stream];
+  expect(result2).toEqual(result);
+});
+
+// This fails. Side 1 gets the first value, side 2 gets the second value.
+// test('forked stream -- both sides see the same values', () => {
+//   const stream = restartableFilter(
+//     ['issue'],
+//     restartable(() => sourceToStream('issue', issueSource)),
+//     _ => true,
+//   );
+
+//   const side1 = stream[Symbol.iterator]();
+//   const side2 = stream[Symbol.iterator]();
+
+//   const side1Results = [];
+//   const side2Results = [];
+//   for (;;) {
+//     const side1Next = side1.next();
+//     const side2Next = side2.next();
+//     if (side1Next.done && side2Next.done) {
+//       break;
+//     }
+//     side1Results.push(side1Next.value);
+//     side2Results.push(side2Next.value);
+//   }
+
+//   expect(side1Results).toEqual(side2Results);
+//   expect(side1Results.length).toEqual(issueSource.length);
+//   expect(side1Results).toEqual([...stream]);
+//   console.log(side1Results);
+// });
