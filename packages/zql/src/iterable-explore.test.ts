afc835b9b585fb33a3a24c220f240e09fbc88301
Matthew Wonlaw <mlaw@roci.dev>
2024-07-31 20:36:15 -0400

    join exploration

--- packages/zql/src/iterable-explore.test.ts
+++ packages/zql/src/iterable-explore.test.ts
@@ -1,44 +1,81 @@
 import {expect, test} from 'vitest';
-import type {Entry} from './zql/ivm/multiset.js';
+import type {JSONObject} from '../../shared/src/json.js';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type TODO = any;
 type Issue = {
   id: number;
   title: string;
 };
-const issueSource: Entry<Issue>[] = [
-  [
-    {
+const event = Symbol();
+type Event = Add | Remove | NoOp;
+const ADD = 1;
+const REMOVE = -1;
+const NO_OP = 0;
+
+type Add = typeof ADD;
+type Remove = typeof REMOVE;
+type NoOp = typeof NO_OP;
+type Entry<Table = string, Type = JSONObject> =
+  | ({
+      [t in Table & string]: Type;
+    } & {[event]: Event})
+  | (Record<string, Iterable<Entry> | JSONObject> & {[event]: Event});
+
+const issueSource: Entry<'issue', Issue>[] = [
+  {
+    issue: {
       id: 1,
       title: 'issue 1',
     },
-    1,
-  ],
-  [
-    {
+    [event]: ADD,
+  },
+  {
+    issue: {
       id: 2,
       title: 'issue 2',
     },
-    1,
-  ],
+    [event]: ADD,
+  },
 ];
 
-type ItemType = {
-  [table: string]: Entry<unknown> | Iterable<ItemType>;
-};
+type Comment = {id: number; issueId: number; text: string};
+const commentSource: Entry<'comment', Comment>[] = [
+  {
+    comment: {
+      id: 1,
+      issueId: 1,
+      text: 'comment 1',
+    },
+    [event]: ADD,
+  },
+  {
+    comment: {
+      id: 2,
+      issueId: 1,
+      text: 'comment 2',
+    },
+    [event]: ADD,
+  },
+  {
+    comment: {
+      id: 3,
+      issueId: 2,
+      text: 'comment 3',
+    },
+    [event]: ADD,
+  },
+];
 
-function* sourceToStream(tableName: string, source: Iterable<Entry<unknown>>) {
-  for (const entry of source) {
-    yield {
-      [tableName]: entry,
-    } satisfies ItemType;
-  }
-}
+type ResultType = {
+  [table: string]: JSONObject | ResultType;
+}[];
 
 function* filter(
   path: string[],
-  iterable: Iterable<ItemType>,
-  cb: (v: Entry<unknown>) => boolean,
-): IterableIterator<ItemType> {
+  iterable: Iterable<Entry>,
+  cb: (v: unknown) => boolean,
+): IterableIterator<Entry> {
   const [head, ...tail] = path;
   for (const row of iterable) {
     if (tail.length === 0) {
@@ -48,13 +85,60 @@ function* filter(
     } else {
       yield {
         ...row,
-        [head]: filter(tail, row[head] as Iterable<ItemType>, cb),
-      } as ItemType;
+        [head]: filter(tail, row[head] as Iterable<Entry>, cb),
+      } as Entry;
     }
   }
 }
 
-function restartable<T>(generatorFunc: () => Iterator<T>) {
+function* loopJoin(
+  left: Iterable<Entry>,
+  right: Iterable<Entry>,
+  leftItemPath: string[],
+  rightItem: string,
+  insertAs: string,
+  cb: (left: unknown, right: unknown) => boolean,
+): IterableIterator<Entry> {
+  // We only ever get the parent most thing of the right, correct?
+  // I think so.. since a query with a sub-query is a leftJoin(parent, child).
+  // So we're always operating on the top level result of the child, right?
+
+  const [leftHead, ...leftTail] = leftItemPath;
+  for (const leftRow of left) {
+    if (leftTail.length === 0) {
+      // nest the right into the left
+      yield {
+        ...leftRow,
+        [insertAs]: (function* () {
+          for (const rightRow of right) {
+            if (
+              cb(
+                leftRow[leftHead] as Entry<unknown>,
+                rightRow[rightItem] as Entry<unknown>,
+              )
+            ) {
+              yield rightRow;
+            }
+          }
+        })(),
+      } as Entry;
+    } else {
+      yield {
+        ...leftRow,
+        [leftHead]: loopJoin(
+          leftRow[leftHead] as Iterable<Entry>,
+          right,
+          leftTail,
+          rightItem,
+          insertAs,
+          cb,
+        ),
+      } as Entry;
+    }
+  }
+}
+
+function restartable<T>(generatorFunc: () => IterableIterator<T>) {
   let iter: Iterator<T>;
   return {
     [Symbol.iterator]() {
@@ -75,30 +159,24 @@ function restartable<T>(generatorFunc: () => Iterator<T>) {
 
 function restartableFilter(
   path: string[],
-  iterable: Iterable<ItemType>,
-  predicate: (v: Entry<unknown>) => boolean,
+  iterable: Iterable<Entry>,
+  predicate: (v: unknown) => boolean,
 ) {
   return restartable(() => filter(path, iterable, predicate));
 }
 
 test('filter against a root source', () => {
   const result = [
-    ...filter(
-      ['issue'],
-      sourceToStream('issue', issueSource),
-      v => (v as Entry<Issue>)[0].title === 'issue 1',
-    ),
+    ...filter(['issue'], issueSource, v => (v as Issue).title === 'issue 1'),
   ];
 
   expect(result).toEqual([
     {
-      issue: [
-        {
-          id: 1,
-          title: 'issue 1',
-        },
-        1,
-      ],
+      issue: {
+        id: 1,
+        title: 'issue 1',
+      },
+      [event]: ADD,
     },
   ]);
 });
@@ -106,27 +184,94 @@ test('filter against a root source', () => {
 test('re-pulling the stream', () => {
   const stream = restartableFilter(
     ['issue'],
-    restartable(() => sourceToStream('issue', issueSource)),
-    v => (v as Entry<Issue>)[0].title === 'issue 1',
+    restartable(() => issueSource[Symbol.iterator]()),
+    v => (v as Issue).title === 'issue 1',
   );
 
   const result = [...stream];
 
   expect(result).toEqual([
     {
-      issue: [
-        {
-          id: 1,
-          title: 'issue 1',
-        },
-        1,
-      ],
+      issue: {
+        id: 1,
+        title: 'issue 1',
+      },
+      [event]: ADD,
     },
   ]);
 
   const result2 = [...stream];
   expect(result2).toEqual(result);
 });
+
+test('loop join', () => {
+  const stream = loopJoin(
+    issueSource,
+    commentSource,
+    ['issue'],
+    'comment',
+    'comments',
+    (left, right) => (left as Issue).id === (right as Comment).issueId,
+  );
+
+  const result = view(stream);
+  expect(result).toEqual([
+    {
+      comments: [
+        {
+          comment: {
+            id: 1,
+            issueId: 1,
+            text: 'comment 1',
+          },
+        },
+        {
+          comment: {
+            id: 2,
+            issueId: 1,
+            text: 'comment 2',
+          },
+        },
+      ],
+      issue: {
+        id: 1,
+        title: 'issue 1',
+      },
+    },
+    {
+      comments: [
+        {
+          comment: {
+            id: 3,
+            issueId: 2,
+            text: 'comment 3',
+          },
+        },
+      ],
+      issue: {
+        id: 2,
+        title: 'issue 2',
+      },
+    },
+  ]);
+});
+
+function view(iterable: Iterable<Entry>): ResultType {
+  const ret: ResultType = [];
+  for (const row of iterable) {
+    const newRow: {[table: string]: JSONObject | ResultType} = {};
+    for (const [key, value] of Object.entries(row)) {
+      if ((value as TODO)[Symbol.iterator]) {
+        newRow[key] = view(value as Iterable<Entry>);
+      } else {
+        newRow[key] = value as Entry<unknown> as JSONObject;
+      }
+    }
+    ret.push(newRow);
+  }
+
+  return ret;
+}
 
 // This fails. Side 1 gets the first value, side 2 gets the second value.
 // test('forked stream -- both sides see the same values', () => {
