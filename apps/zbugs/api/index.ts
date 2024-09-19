87eaf83e7a9be9f8f0ee5764990ebeff52a25aa2
Matthew Wonlaw <mlaw@roci.dev>
2024-09-18 20:23:34 -0400

    feat(zbugs): oauth flow

--- apps/zbugs/api/index.ts
+++ apps/zbugs/api/index.ts
@@ -1,15 +1,65 @@
 // https://vercel.com/templates/other/fastify-serverless-function
 import Fastify from 'fastify';
+import cookie from '@fastify/cookie';
+import oauthPlugin from '@fastify/oauth2';
+import 'dotenv/config';
+import {Octokit} from '@octokit/core';
 
-export const app = Fastify({
+import {OAuth2Namespace} from '@fastify/oauth2';
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    githubOAuth2: OAuth2Namespace;
+  }
+}
+
+export const fastify = Fastify({
   logger: true,
 });
 
-app.get('/api', async (req, reply) => {
+fastify.register(cookie);
+
+fastify.register(oauthPlugin, {
+  name: 'githubOAuth2',
+  credentials: {
+    client: {
+      id: process.env.GITHUB_CLIENT_ID,
+      secret: process.env.GITHUB_CLIENT_SECRET,
+    },
+    auth: oauthPlugin.GITHUB_CONFIGURATION,
+  },
+  startRedirectPath: '/api/login/github',
+  callbackUri: req =>
+    `${req.protocol}://${req.hostname}:${req.port}/api/login/github/callback`,
+});
+
+fastify.get('/api', async (_req, reply) => {
   return reply.status(200).send({hello: 'world'});
 });
 
+// The service provider redirect the user here after successful login
+fastify.get('/api/login/github/callback', async function (request, reply) {
+  const {token} =
+    await this.githubOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
+
+  const octokit = new Octokit({
+    auth: token.access_token,
+  });
+
+  const userDetails = await octokit.request('GET /user', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+  // get user info: https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-the-authenticated-user
+  // 1. Write the user info if it doesn't exist
+  // 2. Write the session to the database
+  // 3. Set the session_id in the cookie
+  // 4. Redirect the user to the dashboard
+  reply.cookie('session_id', 'session_id').send(userDetails);
+});
+
 export default async function handler(req, reply) {
-  await app.ready();
-  app.server.emit('request', req, reply);
+  await fastify.ready();
+  fastify.server.emit('request', req, reply);
 }
--- apps/zbugs/vite.config.ts
+++ apps/zbugs/vite.config.ts
@@ -3,15 +3,15 @@ import {makeDefine} from 'shared/src/build.js';
 import {defineConfig, ViteDevServer} from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {app} from './api/index.js';
+import {fastify} from './api/index.js';
 
 async function configureServer(server: ViteDevServer) {
-  await app.ready();
+  await fastify.ready();
   server.middlewares.use((req, res, next) => {
     if (!req.url.startsWith('/api')) {
       return next();
     }
-    app.server.emit('request', req, res);
+    fastify.server.emit('request', req, res);
   });
 }
 
