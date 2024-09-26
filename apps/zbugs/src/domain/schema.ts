f87986218b9ea591ae3b41714fe161694fb6cd13
Matthew Wonlaw <mlaw@roci.dev>
2024-09-25 21:55:07 -0400

    chore(zbugs): fix export from `schema.ts`

--- apps/zbugs/src/domain/schema.ts
+++ apps/zbugs/src/domain/schema.ts
@@ -1,4 +1,5 @@
 import {createUseZero} from 'zero-react/src/use-zero.js';
-export {Schema, schema} from './schema-shared.js';
-import {Schema} from './schema-shared.js';
+export type {Schema} from './schema-shared.js';
+export {schema} from './schema-shared.js';
+import type {Schema} from './schema-shared.js';
 export const useZero = createUseZero<Schema>();
