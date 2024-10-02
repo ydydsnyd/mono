067ffbb9370e291ca944ada9c163f0f4ddc42483
Matthew Wonlaw <mlaw@roci.dev>
2024-10-02 13:31:02 -0400

    feat(zero-cache): mutation limiter

---
+++ packages/zero-cache/src/services/limiter/sliding-window-limiter.test.ts
@@ -0,0 +1,93 @@
+import {afterEach, beforeEach, expect, test, vi} from 'vitest';
+import {SlidingWindowLimiter} from './sliding-window-limiter.js';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
+test('all mutations occur at prior window start', () => {
+  vi.setSystemTime(10);
+
+  const limiter = new SlidingWindowLimiter(10, 10);
+  for (let i = 0; i < 10; i++) {
+    expect(limiter.canDo()).toBe(true);
+  }
+
+  // 11th call fails
+  expect(limiter.canDo()).toBe(false);
+  // and 12th of course
+  expect(limiter.canDo()).toBe(false);
+
+  // failed limiter calls still bump the count
+  expect(limiter.totalCallsForTime(10)).toBe(12);
+});
+
+// Sliding window setup should look like:
+// |----|----|
+//|----|
+test('all mutations occur at prior window end', () => {
+  // prior window end is start + windowSizeMs - 1
+  vi.setSystemTime(9);
+
+  const limiter = new SlidingWindowLimiter(10, 10);
+  for (let i = 0; i < 10; i++) {
+    expect(limiter.canDo()).toBe(true);
+  }
+
+  // 11th call fails
+  expect(limiter.canDo()).toBe(false);
+});
+
+test('fill the window then slide the window', () => {
+  vi.setSystemTime(9);
+
+  const limiter = new SlidingWindowLimiter(10, 10);
+  for (let i = 0; i < 10; i++) {
+    expect(limiter.canDo()).toBe(true);
+  }
+
+  expect(limiter.totalCallsForTime(9)).toBe(10);
+
+  // sliding out of the past with no new writes should decimate the count
+  for (let i = 0; i < 10; i++) {
+    expect(limiter.totalCallsForTime(10 + i)).toBe(10 - i - 1);
+  }
+
+  // sliding into the future while writing should keep the count constant
+  for (let i = 0; i < 10; i++) {
+    vi.setSystemTime(10 + i);
+    limiter.canDo();
+    expect(limiter.totalCallsForTime(10 + i)).toBe(10);
+  }
+});
+
+test('all mutations occur at next window start', () => {
+  vi.setSystemTime(0);
+
+  const limiter = new SlidingWindowLimiter(10, 10);
+  vi.setSystemTime(10);
+
+  for (let i = 0; i < 10; i++) {
+    expect(limiter.canDo()).toBe(true);
+  }
+
+  // 11th call fails
+  expect(limiter.canDo()).toBe(false);
+});
+test('all mutations occur at next window end', () => {
+  vi.setSystemTime(0);
+
+  const limiter = new SlidingWindowLimiter(10, 10);
+  vi.setSystemTime(19);
+
+  for (let i = 0; i < 10; i++) {
+    expect(limiter.canDo()).toBe(true);
+  }
+
+  // 11th call fails
+  expect(limiter.canDo()).toBe(false);
+});
---
+++ packages/zero-cache/src/services/limiter/sliding-window-limiter.ts
@@ -0,0 +1,152 @@
+import {assert} from 'shared/src/asserts.js';
+
+type Window = {
+  start: number;
+  count: number;
+};
+
+/**
+ * The limiter is a sliding window rate limiter that allows a maximum
+ * number of mutations per window.
+ *
+ * The window is divided into two parts: the prev window and the next window.
+ * The prev window is the part of the window that has already passed, and the
+ * next window is the part of the window that is still in the future.
+ *
+ * The limiter keeps track of the number of mutations in each window. When the
+ * current time moves completely past the next window, the limiter creates new
+ * windows (this only happens if the limiter is not called for a long time).
+ * When the current time moves completely into the next window, the
+ * limiter rotates the windows.
+ *
+ * Case 1: need new windows
+ * |----|----|
+ *             |----| (sliding window)
+ *
+ * Case 2: rotate windows
+ * |----|----|
+ *      |----| (sliding window)
+ *
+ * or
+ *
+ * |----|----|
+ *         |----| (sliding window)
+ *
+ * The leading edge of the sliding window is current time.
+ *
+ * The limiter increments the count for the prev window if the current time is
+ * in the prior window. Otherwise, it increments the count for the next
+ * window.
+ *
+ * The limiter computes the total mutations by: taking the number of mutation in the next window and
+ * adding the number of mutation from the prev window, weighted by
+ * the fraction of the window that has passed. The total mutations must be less
+ * than the maximum number of mutations allowed per window.
+ *
+ * |----|----|
+ *    |----| (sliding window)
+ * ^-- the sliding window covers 25% of the prior window. Only 25% of that window's count counts.
+ * The entirety of the next window's count counts.
+ */
+export class SlidingWindowLimiter {
+  readonly #windowSizeMs: number;
+  readonly #maxMutations: number;
+
+  #priorWindow: Window;
+  #nextWindow: Window;
+
+  constructor(windowSizeMs: number, maxMutations: number) {
+    this.#windowSizeMs = windowSizeMs;
+    this.#maxMutations = maxMutations;
+
+    [this.#priorWindow, this.#nextWindow] = this.#newWindows();
+  }
+
+  canDo(): boolean {
+    const now = Date.now();
+
+    // If the current sliding window is completely past the next window, we need new windows.
+    if (
+      now - this.#windowSizeMs >
+      this.#nextWindow.start + this.#windowSizeMs
+    ) {
+      [this.#priorWindow, this.#nextWindow] = this.#newWindows();
+    }
+
+    // Has the current sliding window moved completely into the next window?
+    // Then rotate the windows.
+    if (now - this.#windowSizeMs >= this.#nextWindow.start) {
+      this.#rotateWindows();
+    }
+
+    // If "now" in the prior or next window? Increment the count for the correct window.
+    if (now < this.#nextWindow.start) {
+      this.#priorWindow.count++;
+    } else {
+      this.#nextWindow.count++;
+    }
+
+    // Now compute the total mutations in the current window.
+    // Weighted by the fraction of the window that has passed.
+    const totalCalls = this.totalCallsForTime(now);
+    return totalCalls <= this.#maxMutations;
+  }
+
+  totalCallsForTime(now: number): number {
+    let fraction: number;
+    if (now < this.#priorWindow.start + this.#windowSizeMs) {
+      fraction = 1;
+    } else {
+      fraction =
+        (this.#priorWindow.start +
+          (this.#windowSizeMs - 1) -
+          (now - this.#windowSizeMs)) /
+        this.#windowSizeMs;
+    }
+    if (fraction < 0) {
+      fraction = 0;
+    }
+    assert(
+      fraction <= 1,
+      'The past cannot contribute more than the a full window.',
+    );
+    const totalCalls =
+      this.#priorWindow.count * fraction + this.#nextWindow.count;
+    return totalCalls;
+  }
+
+  get priorWindow(): Window {
+    return this.#priorWindow;
+  }
+
+  get nextWindow(): Window {
+    return this.#nextWindow;
+  }
+
+  /**
+   * If there were no mutations in over `windowSizeMs` we need new windows.
+   */
+  #newWindows() {
+    const now = Date.now();
+    const start = now - (now % this.#windowSizeMs);
+    return [
+      {
+        // clamp to a `windowSizeMs` boundary to start the prior window.
+        start,
+        count: 0,
+      },
+      {
+        start: start + this.#windowSizeMs,
+        count: 0,
+      },
+    ];
+  }
+
+  #rotateWindows() {
+    this.#priorWindow = this.#nextWindow;
+    this.#nextWindow = {
+      start: this.#priorWindow.start + this.#windowSizeMs,
+      count: 0,
+    };
+  }
+}
