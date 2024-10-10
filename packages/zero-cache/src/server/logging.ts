3568d026ee3ff46b3f5a8aef7e169cd42d02d8be
Darick Tong <132324914+darkgnotic@users.noreply.github.com>
2024-10-09 21:34:05 -0700

    chore(zero-cache): capitalize the log level sent to structured logs (#2620)

--- packages/zero-cache/src/server/logging.ts
+++ packages/zero-cache/src/server/logging.ts
@@ -55,7 +55,7 @@ const consoleJsonLogSink: LogSink = {
 
     console[level](
       stringify({
-        level,
+        level: level.toUpperCase(),
         ...context,
         ...message,
         ...lastObj,
