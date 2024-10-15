16e1a74076d4e79318a5c9f53d0b86ee1e9f6fbc
Erik Arvidsson <arv@roci.dev>
2024-10-15 23:26:44 +0200

    fix(zbugs): no op useKeyPress in inputs
    If the event target is an input (or textarea, select or content
    editable), we should not call the key press handler.

--- apps/zbugs/src/hooks/use-keypress.ts
+++ apps/zbugs/src/hooks/use-keypress.ts
@@ -4,10 +4,14 @@ export function useKeypress(
   key: string,
   callback: () => void,
   event: 'keypress' | 'keyup' | 'keydown' = 'keypress',
+  allowOnInputElements = false,
 ) {
   useEffect(() => {
     function handleKeypress(event: KeyboardEvent) {
-      if (event.key === key) {
+      if (
+        event.key === key &&
+        (allowOnInputElements || shouldAllow(event.target as HTMLElement))
+      ) {
         callback();
       }
     }
@@ -16,5 +20,17 @@ export function useKeypress(
     return () => {
       window.removeEventListener(event, handleKeypress);
     };
-  }, [key, callback]);
+  }, [key, callback, event, allowOnInputElements]);
+}
+
+function shouldAllow(el: HTMLElement): boolean {
+  const tagName = el.tagName;
+  switch (tagName) {
+    case 'TEXTAREA':
+    case 'SELECT':
+    case 'INPUT':
+      // To be fair. We could probably allow on type=radio,button and many more. Add more as
+      return false;
+  }
+  return !el.isContentEditable;
 }
