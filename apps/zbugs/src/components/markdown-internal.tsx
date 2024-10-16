84600f8ed7ecc79e1c2ffc183269dd50368da9c3
Matthew Wonlaw <mlaw@roci.dev>
2024-10-15 22:12:55 -0400

    feat(zbugs): use github flavored markdown

--- apps/zbugs/src/components/markdown-internal.tsx
+++ apps/zbugs/src/components/markdown-internal.tsx
@@ -1,14 +1,9 @@
 import MarkdownBase from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
-import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
 
 /**
  * Do not import this component directly. Use `Markdown` instead.
  */
 export default function Markdown({children}: {children: string}) {
-  return (
-    <MarkdownBase rehypePlugins={[rehypeRaw, rehypeSanitize]}>
-      {children}
-    </MarkdownBase>
-  );
+  return <MarkdownBase rehypePlugins={[remarkGfm]}>{children}</MarkdownBase>;
 }
