eb1c07ca5de398592b9518f15b84322c1f025842
Erik Arvidsson <arv@roci.dev>
2024-10-17 13:40:07 +0200

    chore(zero-client): Run tests cross browser (#2708)
    Run the tests in chromium, firefox, and webkit.

--- packages/shared/src/tool/vitest-config.ts
+++ packages/shared/src/tool/vitest-config.ts
@@ -1,9 +1,27 @@
+import {argv} from 'node:process';
 import {makeDefine} from '../build.js';
 
 const define = {
   ...makeDefine(),
   ['TESTING']: 'true',
 };
+
+/**
+ * Find name from process.argv.
+ *
+ * The argv has --browser.name=chromium which
+ * overrides the test.browser.name in the config but there is no way to read it
+ * at this level so we have to do it manually.
+ */
+function findBrowserName() {
+  for (const arg of argv) {
+    const m = arg.match(/--browser\.name=(.+)/);
+    if (m) {
+      return m[1];
+    }
+  }
+  return undefined;
+}
 
 export const config = {
   // https://github.com/vitest-dev/vitest/issues/5332#issuecomment-1977785593
@@ -17,13 +35,15 @@ export const config = {
   },
 
   test: {
+    name: findBrowserName(),
     onConsoleLog(log: string) {
       if (
         log.includes('Skipping license check for TEST_LICENSE_KEY.') ||
         log.includes('REPLICACHE LICENSE NOT VALID') ||
         log.includes('enableAnalytics false') ||
         log.includes('no such entity') ||
-        log.includes(`Zero starting up with no server URL`)
+        log.includes(`Zero starting up with no server URL`) ||
+        log.includes(`PokeHandler clearing due to unexpected poke error`)
       ) {
         return false;
       }
@@ -42,4 +62,4 @@ export const config = {
     },
     testTimeout: 10_000,
   },
-} as const;
+};
