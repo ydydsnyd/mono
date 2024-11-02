c0c728b3da092915d0293d78b4023b673f62919d
Aaron Boodman <aaron@aaronboodman.com>
2024-11-02 09:16:23 -1000

    wip

--- apps/hello-do/src/index.ts
+++ apps/hello-do/src/index.ts
@@ -1,101 +1,80 @@
 import { DurableObject } from 'cloudflare:workers';
-import { createTableSchema, Zero } from '@rocicorp/zero';
-import { resolver, resolver } from '@rocicorp/resolver';
+import { Zero } from '@rocicorp/zero';
+import { schema, Schema } from './schema';
 
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
-
-/** A Durable Object's behavior is defined in an exported Javascript class */
 export class MyDurableObject extends DurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 */
+	#z: Zero<Schema>;
+	#commentIDs: Set<string> = new Set();
+	#gotFirstResult = false;
+	#count = 0;
+
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
 		addEventListener('unhandledrejection', (e) => {
 			console.log('unhandledrejection', e);
 		});
-	}
 
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
-	async sayHello(name: string): Promise<string> {
-		const userTable = createTableSchema({
-			tableName: 'user',
-			columns: {
-				id: { type: 'string' },
-				login: { type: 'string' },
-				name: { type: 'string' },
-				avatar: { type: 'string' },
-				role: { type: 'string' },
-			},
-			primaryKey: ['id'],
-			relationships: {},
-		});
-		const z = new Zero({
-			server: 'http://localhost:4848',
-			userID: 'anon',
-			schema: {
-				version: 4,
-				tables: { user: userTable },
-			},
+		this.#z = new Zero({
+			server: 'https://zero-service-accel.reflect-server.net',
+			userID: 'OeVnr1y5bEM_Yg06sUFtD',
+			schema,
 			kvStore: 'mem',
-			logLevel: 'debug',
+			auth: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPZVZucjF5NWJFTV9ZZzA2c1VGdEQiLCJpYXQiOjE3MzA1Mjg0NTUsIm5hbWUiOiJhYm9vZG1hbiIsImV4cCI6MTczMzEyMDQ1NX0.lt2EbQkmuZ_MbUgku1avCQp_c0qWx1y9TAlBWjgJajc',
 		});
-		const { promise, resolve } = resolver<string>();
-		const view = z.query.user.materialize();
-		view.addListener((data) => {
-			if (data.length === 0) {
+
+		console.log('issuing query', this.#count++);
+		const view = this.#z.query.issue
+			.related('comments', (c) => c.orderBy('created', 'desc'))
+			.where('shortID', 3109)
+			.one()
+			.materialize();
+		view.addListener((issue) => {
+			if (!issue) {
 				return;
 			}
-			resolve(JSON.stringify(data));
-			view.destroy();
+
+			console.log('issue change', issue);
+			const prevComments = this.#commentIDs;
+			this.#commentIDs = new Set(issue.comments.map((c) => c.id));
+
+			if (!this.#gotFirstResult) {
+				this.#gotFirstResult = true;
+				console.log('Got first result, ignoring');
+			}
+			const latestComment = issue?.comments[0] ?? undefined;
+			if (latestComment && !prevComments.has(latestComment.id)) {
+				console.log('New comment added - adding reply');
+				const id = Math.random().toString(32).substring(2);
+				this.#commentIDs.add(id);
+				const props = ['Woo! awesome 🚀', 'Great job! 👍', "That's amazing! 🎉", 'Wow! 🤯'];
+				const body = props[Math.floor(Math.random() * props.length)];
+				void this.#z.mutate.comment.create({
+					id,
+					issueID: issue.id,
+					created: Date.now(),
+					body,
+					creatorID: this.#z.userID,
+				});
+			} else {
+				console.log('Issue changed for some other reason, ignoring');
+			}
 		});
-		return promise;
+	}
+
+	init() {
+		return 'ok';
 	}
 }
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
-	async fetch(request, env, ctx): Promise<Response> {
-		// We will create a `DurableObjectId` using the pathname from the Worker request
-		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
-
-		// This stub creates a communication channel with the Durable Object instance
-		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub = env.MY_DURABLE_OBJECT.get(id);
-
-		// We call the `sayHello()` RPC method on the stub to invoke the method on the remote
-		// Durable Object instance
-		let greeting = await stub.sayHello('world');
-
-		return new Response(greeting);
+	async fetch(request, env): Promise<Response> {
+		const url = new URL(request.url);
+		if (url.pathname !== '/') {
+			return new Response('Not found', { status: 404 });
+		}
+		const id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
+		const stub = env.MY_DURABLE_OBJECT.get(id);
+		// We only get the DO to trigger it to watch the issue
+		return new Response(await stub.init());
 	},
 } satisfies ExportedHandler<Env>;
---
+++ apps/hello-do/src/schema.ts
@@ -0,0 +1,43 @@
+import { createSchema, createTableSchema, SchemaToRow } from '@rocicorp/zero';
+
+const issueSchema = createTableSchema({
+	tableName: 'issue',
+	columns: {
+		id: { type: 'string' },
+		shortID: { type: 'number', optional: true },
+	},
+	primaryKey: ['id'],
+	relationships: {
+		comments: {
+			source: 'id',
+			dest: {
+				field: 'issueID',
+				schema: () => commentSchema,
+			},
+		},
+	},
+});
+
+const commentSchema = createTableSchema({
+	tableName: 'comment',
+	columns: {
+		id: { type: 'string' },
+		issueID: { type: 'string' },
+		created: { type: 'number' },
+		body: { type: 'string' },
+		creatorID: { type: 'string' },
+	},
+	primaryKey: ['id'],
+	relationships: {},
+});
+
+export const schema = createSchema({
+	version: 4,
+	tables: {
+		issue: issueSchema,
+		comment: commentSchema,
+	},
+});
+
+export type Schema = typeof schema;
+export type Comment = SchemaToRow<typeof commentSchema>;
