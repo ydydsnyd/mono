e92d9b1c063c275f50a0a37d12f710bd0d5eaa90
Darick Tong <132324914+darkgnotic@users.noreply.github.com>
2024-11-05 18:01:14 -0800

    feat(zero-cache): sync url parsing (#2933)

--- packages/zero-cache/src/services/dispatcher/dispatcher.ts
+++ packages/zero-cache/src/services/dispatcher/dispatcher.ts
@@ -1,12 +1,18 @@
 import {LogContext} from '@rocicorp/logger';
 import {IncomingMessage} from 'http';
+import UrlPattern from 'url-pattern';
 import {h32} from '../../../../shared/src/xxhash.js';
 import type {Worker} from '../../types/processes.js';
 import {HttpService, type Options} from '../http-service.js';
 import {getConnectParams} from './connect-params.js';
 import {installWebSocketHandoff} from './websocket-handoff.js';
 
-export const CONNECT_URL_PATTERN = '/api/sync/:version/connect';
+const CONNECT_URL_PATTERNS = [
+  new UrlPattern('/zero/sync/:version/connect'),
+  new UrlPattern('/api/sync/:version/connect'),
+];
+
+const SUPPORTED_VERSION = 'v1';
 
 export type Workers = {
   syncers: Worker[];
@@ -30,11 +36,16 @@ export class Dispatcher extends HttpService {
   }
 
   #handoff(req: IncomingMessage) {
-    const {headers, url} = req;
-    const {params, error} = getConnectParams(
-      new URL(url ?? '', 'http://unused/'),
-      headers,
-    );
+    const {headers, url: u} = req;
+    const url = new URL(u ?? '', 'http://unused/');
+    const syncPath = parseSyncPath(url);
+    if (!syncPath) {
+      throw new Error(`Invalid sync URL: ${u}`);
+    }
+    if (syncPath.version !== SUPPORTED_VERSION) {
+      throw new Error(`Unsupported sync version: ${u}`);
+    }
+    const {params, error} = getConnectParams(url, headers);
     if (error !== null) {
       throw new Error(error);
     }
@@ -49,4 +60,14 @@ export class Dispatcher extends HttpService {
     this._lc.debug?.(`connecting ${clientGroupID} to syncer ${syncer}`);
     return {payload: params, receiver: syncers[syncer]};
   }
+}
+
+function parseSyncPath(url: URL): {version: string} | undefined {
+  for (const pattern of CONNECT_URL_PATTERNS) {
+    const m = pattern.match(url.pathname);
+    if (m) {
+      return m;
+    }
+  }
+  return undefined;
 }
