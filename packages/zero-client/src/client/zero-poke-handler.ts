82ca6648bc87b3ee56feb052db928ace25fd2076
Erik Arvidsson <arv@roci.dev>
2024-11-07 11:35:56 +0100

    fix(zero-client): Add fallback for raf for DO (#2943)
    Conditionally use requestAnimationFrame if it is available, otherwise
    fallback to setTimeout.
    
    This is needed for Cloudflare Durable Objects, which does not have
    requestAnimationFrame.

--- packages/zero-client/src/client/zero-poke-handler.ts
+++ packages/zero-client/src/client/zero-poke-handler.ts
@@ -5,6 +5,7 @@ import type {
   PokeInternal,
 } from '../../../replicache/src/impl.js';
 import type {ClientID, PatchOperation} from '../../../replicache/src/mod.js';
+import {getBrowserGlobal} from '../../../shared/src/browser-env.js';
 import type {
   ClientsPatchOp,
   PokeEndBody,
@@ -48,6 +49,8 @@ export class PokeHandler {
   // order poke errors.
   readonly #pokeLock = new Lock();
   readonly #schema: NormalizedSchema;
+
+  readonly #raf = getBrowserGlobal('requestAnimationFrame') ?? rafFallback;
 
   constructor(
     replicachePoke: (poke: PokeInternal) => Promise<void>,
@@ -119,7 +122,7 @@ export class PokeHandler {
   #startPlaybackLoop() {
     this.#lc.debug?.('starting playback loop');
     this.#pokePlaybackLoopRunning = true;
-    requestAnimationFrame(this.#rafCallback);
+    this.#raf(this.#rafCallback);
   }
 
   #rafCallback = async () => {
@@ -129,7 +132,7 @@ export class PokeHandler {
       this.#pokePlaybackLoopRunning = false;
       return;
     }
-    requestAnimationFrame(this.#rafCallback);
+    this.#raf(this.#rafCallback);
     const start = performance.now();
     rafLC.debug?.(
       'raf fired, processing pokes.  Since last raf',
@@ -340,4 +343,12 @@ function rowsPatchOpToReplicachePatchOp(
     default:
       throw new Error('to be implemented');
   }
+}
+
+/**
+ * Some environments we run in don't have `requestAnimationFrame` (such as
+ * Node, Cloudflare Workers).
+ */
+function rafFallback(callback: () => void): void {
+  setTimeout(callback, 0);
 }
