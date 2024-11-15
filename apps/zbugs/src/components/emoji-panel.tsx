7bf39432858fa0638b11a5263a17e3e62ea935d8
Erik Arvidsson <arv@roci.dev>
2024-11-15 16:22:42 +0100

    feat(zbugs): Add emoji who reacted tooltip (#3014)
    This still uses the title attribute, but it's a start.

--- apps/zbugs/src/components/emoji-panel.tsx
+++ apps/zbugs/src/components/emoji-panel.tsx
@@ -8,13 +8,15 @@ import type {TableSchemaToRow} from '@rocicorp/zero';
 import {nanoid} from 'nanoid';
 import {useCallback} from 'react';
 import {useQuery} from 'zero-react/src/use-query.js';
-import addEmojiIcon from '../assets/icons/add-emoji.svg';
 import type {Schema} from '../../schema.js';
+import addEmojiIcon from '../assets/icons/add-emoji.svg';
 import {useLogin} from '../hooks/use-login.js';
 import {useNumericPref} from '../hooks/use-user-pref.js';
 import {useZero} from '../hooks/use-zero.js';
 import {ButtonWithLoginCheck} from './button-with-login-check.js';
 import {EmojiPicker, SKIN_TONE_PREF} from './emoji-picker.js';
+
+const loginMessage = 'You need to be logged in to modify emoji reactions.';
 
 type Emoji = TableSchemaToRow<Schema['tables']['emoji']> & {
   creator: TableSchemaToRow<Schema['tables']['user']> | undefined;
@@ -33,8 +35,6 @@ export function EmojiPanel({issueID, commentID}: Props) {
     .related('creator', creator => creator.one());
 
   const emojis: Emoji[] = useQuery(q);
-
-  const skinTone = useNumericPref(SKIN_TONE_PREF, 0);
 
   const addEmoji = useCallback(
     (unicode: string, annotation: string) => {
@@ -77,7 +77,6 @@ export function EmojiPanel({issueID, commentID}: Props) {
   );
 
   const login = useLogin();
-  const loginMessage = 'You need to be logged in to modify emoji reactions.';
 
   const button = (
     <ButtonWithLoginCheck
@@ -92,24 +91,12 @@ export function EmojiPanel({issueID, commentID}: Props) {
   return (
     <div className="flex gap-2 items-center emoji-reaction-container">
       {Object.entries(groups).map(([normalizedEmoji, emojis]) => (
-        <ButtonWithLoginCheck
-          className="emoji-pill"
-          eventName="Add to existing emoji reaction"
+        <EmojiPill
           key={normalizedEmoji}
-          title={'TODO: Who reacted with this emoji'}
-          loginMessage={loginMessage}
-          onAction={() =>
-            addOrRemoveEmoji({
-              unicode: setSkinTone(normalizedEmoji, skinTone),
-              annotation: emojis[0].annotation ?? '',
-            })
-          }
-        >
-          {unique(emojis).map(value => (
-            <span key={value}>{value}</span>
-          ))}
-          {' ' + emojis.length}
-        </ButtonWithLoginCheck>
+          normalizedEmoji={normalizedEmoji}
+          emojis={emojis}
+          addOrRemoveEmoji={addOrRemoveEmoji}
+        />
       ))}
 
       {login.loginState !== undefined ? (
@@ -188,4 +175,70 @@ function setSkinTone(emoji: string, skinTone: number): string {
 
   // Skin tone modifiers range from U+1F3FB to U+1F3FF
   return normalizedEmoji + String.fromCodePoint(0x1f3fa + skinTone);
+}
+
+type AddOrRemoveEmoji = (details: {
+  unicode: string;
+  annotation: string;
+}) => void;
+
+function EmojiPill({
+  normalizedEmoji,
+  emojis,
+  addOrRemoveEmoji,
+}: {
+  normalizedEmoji: string;
+  emojis: Emoji[];
+  addOrRemoveEmoji: AddOrRemoveEmoji;
+}) {
+  const z = useZero();
+  const skinTone = useNumericPref(SKIN_TONE_PREF, 0);
+
+  // TODO: Richer tooltip
+
+  return (
+    <ButtonWithLoginCheck
+      className="emoji-pill"
+      eventName="Add to existing emoji reaction"
+      key={normalizedEmoji}
+      title={getTooltipText(emojis, z.userID)}
+      loginMessage={loginMessage}
+      onAction={() =>
+        addOrRemoveEmoji({
+          unicode: setSkinTone(normalizedEmoji, skinTone),
+          annotation: emojis[0].annotation ?? '',
+        })
+      }
+    >
+      {unique(emojis).map(value => (
+        <span key={value}>{value}</span>
+      ))}
+      {' ' + emojis.length}
+    </ButtonWithLoginCheck>
+  );
+}
+
+function getTooltipNames(emojis: Emoji[], currentUserID: string): string {
+  const names = emojis.map((emoji, i) => {
+    const capitalizeIfFirst = (s: string) =>
+      i === 0 ? s[0].toUpperCase() + s.slice(1) : s;
+
+    const {creator} = emoji;
+    if (!creator) {
+      return capitalizeIfFirst('unknown');
+    }
+    if (emoji.creatorID === currentUserID) {
+      return capitalizeIfFirst('you');
+    }
+    return capitalizeIfFirst(creator.login);
+  });
+  if (names.length === 1) {
+    return names[0];
+  }
+  return names.slice(0, -1).join(', ') + ' and ' + names.slice(-1);
+}
+
+function getTooltipText(emojis: Emoji[], currentUserID: string): string {
+  const names = getTooltipNames(emojis, currentUserID);
+  return `${names} reacted with ${emojis[0].annotation}`;
 }
