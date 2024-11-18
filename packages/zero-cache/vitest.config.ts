5a73679b99cd78c0568b8bc75fcd3b35fd0921b3
Darick Tong <132324914+darkgnotic@users.noreply.github.com>
2024-11-18 10:05:29 -0800

    chore(zero-cache): disable coverage in continuous integration (#3043)

--- packages/zero-cache/vitest.config.ts
+++ packages/zero-cache/vitest.config.ts
@@ -3,13 +3,15 @@ import {config} from '../shared/src/tool/vitest-config.js';
 
 const {define, esbuild} = config;
 
+const ci = process.env['CI'] === 'true' || process.env['CI'] === '1';
+
 export default defineConfig({
   define,
   esbuild,
   test: {
     reporters: 'basic',
     coverage: {
-      enabled: true,
+      enabled: !ci, // Don't run coverage in continuous integration.
       reporter: [['html'], ['clover', {file: 'coverage.xml'}]],
       include: ['src/**'],
     },
