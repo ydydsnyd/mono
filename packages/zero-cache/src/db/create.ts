97247bf8d07e1c202a87e151bc6f6a3524e95f79
Matthew Wonlaw <mlaw@roci.dev>
2024-11-19 09:27:51 -0500

    feat(zero-cache): do not replicate `not null` constraints

--- packages/zero-cache/src/db/create.ts
+++ packages/zero-cache/src/db/create.ts
@@ -20,15 +20,36 @@ export function columnDef(spec: ColumnSpec) {
   return parts.join('');
 }
 
+function isLiteTableSpec(
+  spec: TableSpec | LiteTableSpec,
+): spec is LiteTableSpec {
+  return !('schema' in spec);
+}
+
 /**
  * Constructs a `CREATE TABLE` statement for a {@link TableSpec}.
  */
 export function createTableStatement(spec: TableSpec | LiteTableSpec): string {
   // Note: DEFAULT expressions are ignored for CREATE TABLE statements,
   // as in that case, row values always come from the replication stream.
+  //
+  // Note: NOT NULL constraints are also ignored for SQLite (replica) tables.
+  // 1. They are enforced by the replication stream.
+  // 2. We need nullability for columns with defaults to support
+  // write permissions on the "proposed mutation" state. Proposed
+  // mutations are written to SQLite in a `BEGIN CONCURRENT` transaction in mutagen.
+  // Permission policies are run against that state (to get their ruling) then the
+  // transaction is rolled back.
   const defs = Object.entries(spec.columns)
     .sort(([_a, {pos: a}], [_b, {pos: b}]) => a - b)
-    .map(([name, spec]) => `${id(name)} ${columnDef({...spec, dflt: null})}`);
+    .map(
+      ([name, columnSpec]) =>
+        `${id(name)} ${columnDef({
+          ...columnSpec,
+          notNull: isLiteTableSpec(spec) ? null : columnSpec.notNull,
+          dflt: null,
+        })}`,
+    );
   if (spec.primaryKey) {
     defs.push(`PRIMARY KEY (${idList(spec.primaryKey)})`);
   }
