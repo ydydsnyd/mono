6d14cab7b96bb30ced30da636cab7e849834619c
Cesar Alaestante <cesar@roci.dev>
2024-12-04 00:45:59 -0700

    use jwt in sandbox test

--- apps/zbugs/playwright/smoke.spec.ts
+++ apps/zbugs/playwright/smoke.spec.ts
@@ -1,21 +1,38 @@
 import {test} from '@playwright/test';
 
-test('loadtest', async ({page}) => {
-  test.setTimeout(75000);
+test('loadtest', async ({page, browser, context}) => {
+  test.setTimeout(700000);
+  await page.context().addCookies([
+    {
+      name: 'jwt',
+      value:
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwRnczbjZFUVM4bXpFMTI2QUZKeDAiLCJpYXQiOjE3MzMyOTc5MTUsInJvbGUiOiJjcmV3IiwibmFtZSI6InJvY2lib3QiLCJleHAiOjE4MTk2OTc5MTV9.mmBeGC_r6y1p3YFqnMN5fRmwm5dBAOHBJVPVHIfOSNA',
+      domain: 'bugs-sandbox.rocicorp.dev',
+      path: '/',
+      expires: -1,
+      httpOnly: false,
+    },
+  ]);
   const testID = Math.random().toString(36).substring(2, 8);
 
-  await page.goto('https://zbugs.vercel.app/');
+  await page.goto('https://bugs-sandbox.rocicorp.dev/');
   await page.getByLabel('VISITOR PASSWORD').click();
   await page.getByLabel('VISITOR PASSWORD').fill('zql');
   await page.getByLabel('VISITOR PASSWORD').press('Enter');
 
-  const delay = Math.random() * 30000;
-  console.log(testID, `Delaying for ${delay}ms to create jitter`);
+  const DELAY_START = 120000;
+  const DELAY_PER_ITERATION = 10000;
+  const NUM_ITERATIONS = 30;
+
+  const delay = Math.random() * DELAY_START;
   await page.waitForTimeout(delay);
 
   const start = Date.now();
   const cgID = await page.evaluate('window.z.clientGroupID');
-  for (let i = 0; i < 10; i++) {
+  console.log(cgID, `Delaying for ${delay}ms to create jitter`);
+
+
+  for (let i = 0; i < NUM_ITERATIONS; i++) {
     console.log(cgID, `Iteration: ${i}`);
     await page.waitForSelector('.issue-list .row');
     console.log(cgID, 'Filtering by open');
@@ -27,40 +44,35 @@ test('loadtest', async ({page}) => {
     await page.locator('.add-filter').click();
     await page.getByText('Filtered by:+ Filter').click();
     await page.getByRole('button', {name: '+ Filter'}).click();
-    await page
-      .locator('div')
-      .filter({hasText: /^CreatorSelect$/})
-      .getByRole('button')
-      .click();
+    // select creator
+    await page.locator('div.add-filter-modal > div:nth-child(1)').click();
+
     let elm = await page.locator(
-      `.filter-modal-item:nth-child(1) .dropdown button:nth-child(${
-        Math.floor(Math.random() * 10) + 1
-      })`,
+      `#options-listbox > li:nth-child(${Math.floor(Math.random() * 5) + 2})`,
     );
+
     console.log(cgID, `Filtering by ${await elm.allTextContents()}`);
     await elm.click();
-
     await page.getByRole('button', {name: '+ Filter'}).click();
-    await page
-      .locator('div')
-      .filter({hasText: /^LabelSelect$/})
-      .getByRole('button')
-      .click();
+    // select assignee
+    await page.locator('div.add-filter-modal > div:nth-child(2)').click();
     elm = await page.locator(
-      `.filter-modal-item:nth-child(2) .dropdown button:nth-child(${
-        Math.floor(Math.random() * 10) + 1
-      })`,
+      `#options-listbox > li:nth-child(${Math.floor(Math.random() * 5) + 2})`,
     );
+
     console.log(cgID, `Filtering by ${await elm.allTextContents()}`);
     await elm.click();
     console.log(cgID, 'Removing user filter');
-    await page.locator('.list-view-filter-container .pill.user').click();
+    await page
+      .locator('.list-view-filter-container .pill.user')
+      .first()
+      .click();
     console.log(cgID, 'Removing label filter');
-    await page.locator('.list-view-filter-container .pill.label').click();
+    await page.locator('.list-view-filter-container .pill.user').last().click();
     await page.locator('.nav-item', {hasText: 'All'}).click();
     await page
       .locator(
-        `.issue-list .row:nth-child(${Math.floor(Math.random() * 5) + 1})`,
+        `.issue-list .row:nth-child(${Math.floor(Math.random() * 5) + 1}) > a`,
       )
       .click();
     await page
@@ -68,7 +80,7 @@ test('loadtest', async ({page}) => {
       .click();
     await page
       .locator(
-        '.issue-sidebar .sidebar-item:first-child .dropdown button:nth-child(2)',
+        '.issue-sidebar .sidebar-item:first-child #options-listbox > li:nth-child(2)',
       )
       .click();
     await page
@@ -76,13 +88,19 @@ test('loadtest', async ({page}) => {
       .click();
     await page
       .locator(
-        '.issue-sidebar .sidebar-item:first-child .dropdown button:nth-child(2)',
+        '.issue-sidebar .sidebar-item:first-child #options-listbox > li:nth-child(2)',
       )
       .click();
     await page.goBack();
+    await page.waitForTimeout(DELAY_PER_ITERATION);
+
   }
+  await context.close();
   await page.close();
-  const elapsed = Date.now() - start;
+  await browser.close();
+  let elapsed = Date.now() - start;
+  elapsed = elapsed - DELAY_PER_ITERATION * NUM_ITERATIONS;
   console.log(`${cgID} loadtest completed in ${(elapsed / 1000).toFixed(2)} secs`);
+  console.log(testID, `Ending Test`);
   console.log(cgID, `Done`);
 });
