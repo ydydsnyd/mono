f976d66315cddbbd14a3f4d892c222645d64329e
Cesar Alaestante <cesar@roci.dev>
2024-12-10 02:14:42 -0700

    feat(zero-cache): cloudformation prod (#3248)

---
+++ packages/zero-cache/start-dual.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Check if the database file exists
+if [ ! -f "$ZERO_REPLICA_FILE" ]; then
+  echo "Database file does not exist. Restoring from backup..."
+  litestream restore -if-db-not-exists -if-replica-exists -o "$ZERO_REPLICA_FILE" "$REPLICA_URL"
+else
+  echo "Database file already exists. Skipping restore."
+fi
+
+if [ "$ZERO_LITESTREAM" = "1" ] || [ "$ZERO_LITESTREAM" = "true" ]; then
+  echo "Launching Litestream"
+  litestream replicate -config /opt/app/prod/zbugs/litestream.yml
+else 
+  echo "Not Launching Litestream and running main"
+  npx tsx ./src/server/main.ts
+fi
+
+
+
