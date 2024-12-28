9d20870e25bf41e5ef1a4b111a0f8b76c165841d
Gregory Baker <greg@roci.dev>
2024-12-28 12:33:55 -0700

    feat(zero-solid): Add resultType support to zero-solid

--- packages/zero-advanced/src/mod.ts
+++ packages/zero-advanced/src/mod.ts
@@ -1,7 +1,7 @@
 export type {Change} from '../../zql/src/ivm/change.js';
 export type {Input, Output} from '../../zql/src/ivm/operator.js';
 export {applyChange} from '../../zql/src/ivm/view-apply-change.js';
-export type {Entry, Format, View} from '../../zql/src/ivm/view.js';
+export type {Entry, Format, View, ViewFactory} from '../../zql/src/ivm/view.js';
 export type {AdvancedQuery} from '../../zql/src/query/query-internal.js';
 export type {Query, QueryType, Smash} from '../../zql/src/query/query.js';
 export type {TableSchema} from '../../zero-schema/src/table-schema.js';
